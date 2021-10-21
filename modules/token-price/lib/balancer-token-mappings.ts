a7009b01a419e55ae9eedafd487dc1734c22bbe6
danielmkm <danielbeets@protonmail.com>
2021-10-21 10:44:13 +0200

    add mooBifi as a balancer priced token

--- modules/token-price/lib/balancer-token-mappings.ts
+++ modules/token-price/lib/balancer-token-mappings.ts
@@ -20,6 +20,7 @@ export const balancerTokenMappings: {
         '0xbf4906762c38f50bc7be0a11bb452c944f6c72e1',
         '0x4a13a2cf881f5378def61e430139ed26d843df9a',
         '0xddc0385169797937066bbd8ef409b5b3c0dfeb52',
+        '0xbf07093ccd6adfc3deb259c557b61e94c1f66945',
     ],
     tokenSymbolOverwrites: {
         miMATIC: 'MAI',
