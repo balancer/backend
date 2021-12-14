086c82473e1b71b8df1b0f1606903a4bbba6f04e
0xSkly <skly.beets@protonmail.com>
2021-12-14 13:58:37 +0100

    implement queries

--- modules/lbp-events/lbp-events.resolvers.ts
+++ modules/lbp-events/lbp-events.resolvers.ts
@@ -1,8 +1,20 @@
 import { Resolvers } from '../../schema';
 import { createLbpEvent, updateLbpEvent } from './lbp-events';
+import { prisma } from '../prisma/prisma-client';
 
 const lbpEventsResolvers: Resolvers = {
-    Query: {},
+    Query: {
+        lbpEvent: async (parent, args) => {
+            return prisma.lbpEvent.findUnique({
+                where: { id: args.id },
+                include: { admins: true },
+                rejectOnNotFound: true,
+            });
+        },
+        lbpEvents: () => {
+            return prisma.lbpEvent.findMany({ include: { admins: true } });
+        },
+    },
     Mutation: {
         lbpEventCreate: async (parent, { lbpEvent, signature }) => {
             return createLbpEvent(lbpEvent, signature);
