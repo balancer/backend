2449b3ec995f7c4b824544fd551f4513bdd308fd
Daniel <danielBeets@protonmail.com>
2021-12-26 11:06:09 +0100

    add cached pools request back in

--- modules/portfolio/portfolio.resolvers.ts
+++ modules/portfolio/portfolio.resolvers.ts
@@ -24,6 +24,9 @@ const resolvers: Resolvers = {
 
             return portfolioHistoryData.map((data) => portfolioService.mapPortfolioDataToGql(data));
         },
+        getCachedPools: async (parent, {}, context) => {
+            return portfolioService.getCachedPools();
+        },
     },
     //we're forced to have at least one mutation
     Mutation: {
