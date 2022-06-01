9c8990f7ba3edf0b3d5a60f9d2d77f1cbcc1c880
Franz <franzns11@gmail.com>
2022-06-01 17:22:59 +0200

    prisma client

--- modules/util/sentry-client.ts
+++ modules/util/sentry-client.ts
@@ -3,15 +3,13 @@ import * as Tracing from '@sentry/tracing';
 import { env } from '../../app/env';
 import { prisma } from './prisma-client';
 
+const client = prisma;
+
 Sentry.init({
     dsn: env.SENTRY_DSN,
     tracesSampleRate: 1.0,
     debug: true,
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        // @ts-ignore
-        new Tracing.Integrations.Prisma({ prisma }),
-    ],
+    integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Prisma({ client })],
 });
 
 export const sentry = Sentry;
