0567391c6d6551bf749bb14b53945a80fee44aea
Franz <franzns11@gmail.com>
2022-07-11 17:09:16 +0200

    trigger

--- app/scheduleWorkerTasks.ts
+++ app/scheduleWorkerTasks.ts
@@ -50,6 +50,7 @@ function scheduleJob(
             sentry.captureException(new Error(`${taskName} already running, skipping call...`));
             return;
         }
+        //
 
         // const transaction = sentry.startTransaction({
         //     op: 'cron',
