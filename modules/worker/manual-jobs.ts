8489ceb91f0f15be157ea8736051a206561f9771
0xSkly <skly.beets@protonmail.com>
2022-07-28 14:25:37 +0200

    handle dedublication

--- modules/worker/manual-jobs.ts
+++ modules/worker/manual-jobs.ts
@@ -7,13 +7,13 @@ export type WorkerJob = {
 };
 
 async function scheduleJobWithMinimumInterval(job: WokerJobType, minIntervalMs: number): Promise<void> {
-    await workerQueue.sendWithMinimumInterval(JSON.stringify({ type: job }), minIntervalMs);
+    await workerQueue.sendWithMinimumInterval(JSON.stringify({ type: job }), minIntervalMs, job);
 }
 
 // all jobs requiring manual scheduling will be handled here (e.g. sub minute crons)
 
 export async function scheduleManualJobs(): Promise<void> {
-    await scheduleJobWithMinimumInterval('sync-pools', 5000);
-    await scheduleJobWithMinimumInterval('user-sync-wallet-balances-for-all-pools', 1000);
-    await scheduleJobWithMinimumInterval('user-sync-staked-balances', 1000);
+    await scheduleJobWithMinimumInterval('sync-pools', 10000);
+    await scheduleJobWithMinimumInterval('user-sync-wallet-balances-for-all-pools', 5000);
+    await scheduleJobWithMinimumInterval('user-sync-staked-balances', 5000);
 }
--- modules/worker/queue.ts
+++ modules/worker/queue.ts
@@ -5,26 +5,27 @@ import { env } from '../../app/env';
 class WokerQueue {
     constructor(private readonly client: SQSClient, private readonly queueUrl: string) {}
 
-    public async sendMessage(json: string, delaySeconds?: number): Promise<void> {
+    public async sendMessage(json: string, deDublicationId?: string, delaySeconds?: number): Promise<void> {
         const input: SendMessageCommandInput = {
             QueueUrl: this.queueUrl,
             MessageBody: json,
+            MessageDeduplicationId: deDublicationId,
             DelaySeconds: delaySeconds,
         };
         const command = new SendMessageCommand(input);
         await this.client.send(command);
     }
-    public async sendWithMinimumInterval(json: string, minIntervalMs: number): Promise<void> {
+    public async sendWithMinimumInterval(json: string, minIntervalMs: number, deDuplicationId?: string): Promise<void> {
         const startTime = Date.now();
         try {
-            await this.sendMessage(json);
+            await this.sendMessage(json, deDuplicationId);
         } catch (error) {
             console.log(error);
             Sentry.captureException(error);
         } finally {
             const delay = minIntervalMs - (Date.now() - startTime);
             setTimeout(() => {
-                this.sendWithMinimumInterval(json, minIntervalMs);
+                this.sendWithMinimumInterval(json, minIntervalMs, deDuplicationId);
             }, Math.max(0, delay));
         }
     }
