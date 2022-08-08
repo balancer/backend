fd4f7214296a2be6c31b7eb5a5d3690257ccf42a
0xSkly <skly.beets@protonmail.com>
2022-08-08 15:58:06 +0200

    reduce sentry sampling rate

--- app.ts
+++ app.ts
@@ -27,7 +27,7 @@ async function startServer() {
 
     Sentry.init({
         dsn: env.SENTRY_DSN,
-        tracesSampleRate: 1,
+        tracesSampleRate: 0.2,
         environment: env.NODE_ENV,
         enabled: env.NODE_ENV === 'production',
         integrations: [
