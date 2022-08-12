3a67e6a224c2375edcfabab6d083c59b047d37cf
Franz <franzns11@gmail.com>
2022-08-12 19:14:13 +0300

    rate limit higher, bigger cron interval

--- modules/coingecko/coingecko.service.ts
+++ modules/coingecko/coingecko.service.ts
@@ -29,7 +29,7 @@ interface MappedToken {
    that happen.
 
 */
-const requestRateLimiter = new RateLimiter({ tokensPerInterval: 10, interval: 'minute' });
+const requestRateLimiter = new RateLimiter({ tokensPerInterval: 15, interval: 'minute' });
 
 export class CoingeckoService {
     private readonly baseUrl: string;
