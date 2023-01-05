a4003106cf33f5739abdac4c143ef6f5d602142a
franz <franzns11@gmail.com>
2023-01-05 14:24:36 +0100

    change subgraph to real reliquary

--- modules/config/network-config.ts
+++ modules/config/network-config.ts
@@ -122,7 +122,7 @@ const AllNetworkConfigs: { [chainId: string]: NetworkConfig } = {
             beetsBar: 'https://api.thegraph.com/subgraphs/name/beethovenxfi/beets-bar',
             blocks: 'https://api.thegraph.com/subgraphs/name/beethovenxfi/fantom-blocks',
             masterchef: 'https://api.thegraph.com/subgraphs/name/beethovenxfi/masterchefv2',
-            reliquary: 'https://api.thegraph.com/subgraphs/name/beethovenxfi/reliquary-test',
+            reliquary: 'https://api.thegraph.com/subgraphs/name/beethovenxfi/reliquary',
             userBalances: 'https://api.thegraph.com/subgraphs/name/beethovenxfi/user-bpt-balances-fantom',
         },
         eth: {
