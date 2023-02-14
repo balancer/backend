0bfb8bab8a0a2e46d6781e4fa749eced7e611d48
franz <franzns11@gmail.com>
2023-02-14 15:06:04 +0100

    namespace typo

--- modules/metrics/cron.metric.ts
+++ modules/metrics/cron.metric.ts
@@ -1,5 +1,5 @@
 import { CloudwatchMetricsPublisher } from './metrics.client';
 
 export function getCronMetricsPublisher(chainId: string): CloudwatchMetricsPublisher {
-    return new CloudwatchMetricsPublisher(`Backend-${chainId}/CronRuns)`);
+    return new CloudwatchMetricsPublisher(`Backend-${chainId}/CronRuns`);
 }
