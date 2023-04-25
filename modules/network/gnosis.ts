594d5c9c98866d0c3f02c956c2f55e68600e5bb6
Tim Robinson <tim@timjrobinson.com>
2023-04-25 18:02:36 +1000

    Initial pass adding Gnosis Chain

---
+++ modules/network/gnosis.ts
@@ -0,0 +1,249 @@
+import { BigNumber, ethers } from 'ethers';
+import { NetworkConfig, NetworkData } from './network-config-types';
+import { tokenService } from '../token/token.service';
+import { WstethAprService } from '../pool/lib/apr-data-sources/optimism/wsteth-apr.service';
+import { ReaperCryptAprService } from '../pool/lib/apr-data-sources/reaper-crypt-apr.service';
+import { PhantomStableAprService } from '../pool/lib/apr-data-sources/phantom-stable-apr.service';
+import { BoostedPoolAprService } from '../pool/lib/apr-data-sources/boosted-pool-apr.service';
+import { SwapFeeAprService } from '../pool/lib/apr-data-sources/swap-fee-apr.service';
+import { GaugeAprService } from '../pool/lib/apr-data-sources/ve-bal-guage-apr.service';
+import { GaugeStakingService } from '../pool/lib/staking/gauge-staking.service';
+import { BptPriceHandlerService } from '../token/lib/token-price-handlers/bpt-price-handler.service';
+import { LinearWrappedTokenPriceHandlerService } from '../token/lib/token-price-handlers/linear-wrapped-token-price-handler.service';
+import { SwapsPriceHandlerService } from '../token/lib/token-price-handlers/swaps-price-handler.service';
+import { UserSyncGaugeBalanceService } from '../user/lib/user-sync-gauge-balance.service';
+import { every } from '../../worker/intervals';
+import { GithubContentService } from '../content/github-content.service';
+import { gaugeSubgraphService } from '../subgraphs/gauge-subgraph/gauge-subgraph.service';
+
+const gnosisNetworkData: NetworkData = {
+    chain: {
+        slug: 'gnosis',
+        id: 100,
+        nativeAssetAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+        wrappedNativeAssetAddress: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d',
+        prismaId: 'GNOSIS',
+        gqlId: 'GNOSIS',
+    },
+    subgraphs: {
+        startDate: '2021-08-23',
+        balancer: 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gnosis-chain-v2',
+        beetsBar: 'https://',
+        blocks: '',
+        gauge: 'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gauges-gnosis-chain-b',
+        userBalances: 'https://',
+    },
+    eth: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        addressFormatted: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+        symbol: 'xDAI',
+        name: 'xDAI',
+    },
+    weth: {
+        address: '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d',
+        addressFormatted: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d',
+    },
+    coingecko: {
+        nativeAssetId: 'xdai',
+        platformId: 'xdai',
+    },
+    tokenPrices: {
+        maxHourlyPriceHistoryNumDays: 100,
+    },
+    rpcUrl: 'https://poa-xdai.gateway.pokt.network/v1/lb/91bc0e12a76e7a84dd76189d',
+    rpcMaxBlockRange: 2000,
+    beetsPriceProviderRpcUrl: '',
+    sanity: {
+        projectId: '',
+        dataset: '',
+    },
+    protocolToken: 'bal',
+    beets: {
+        address: '0x0000000000000000000000000000000000000000',
+    },
+    bal: {
+        address: '0x7eF541E2a22058048904fE5744f9c7E4C57AF717',
+    },
+    balancer: {
+        vault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+        composableStablePoolFactories: [
+            '0xf23b4DB826DbA14c0e857029dfF076b1c0264843',
+        ],
+        weightedPoolV2Factories: [
+            '0x6CaD2ea22BFA7F4C14Aae92E47F510Cd5C509bc7',
+        ],
+        poolsInRecoveryMode: [],
+        swapProtocolFeePercentage: 0.5,
+        yieldProtocolFeePercentage: 0.5,
+    },
+    multicall: '0xbb6fab6b627947dae0a75808250d8b2652952cb5',
+    masterchef: {
+        address: '0x0000000000000000000000000000000000000000',
+        excludedFarmIds: [],
+    },
+    avgBlockSpeed: 1,
+    sor: {
+        main: {
+            url: '',
+            maxPools: 8,
+            forceRefresh: false,
+            gasPrice: BigNumber.from(10),
+            swapGas: BigNumber.from('1000000'),
+        },
+        canary: {
+            url: '',
+            maxPools: 8,
+            forceRefresh: false,
+            gasPrice: BigNumber.from(10),
+            swapGas: BigNumber.from('1000000'),
+        },
+    },
+    yearn: {
+        vaultsEndpoint: 'https://#/',
+    },
+    reaper: {
+        linearPoolFactories: [''],
+        averageAPRAcrossLastNHarvests: 2,
+    },
+    lido: {
+        wstEthAprEndpoint: '',
+        wstEthContract: '',
+    },
+    datastudio: {
+        main: {
+            user: 'datafeed-service@datastudio-366113.iam.gserviceaccount.com',
+            sheetId: '11anHUEb9snGwvB-errb5HvO8TvoLTRJhkDdD80Gxw1Q',
+            databaseTabName: 'Database v2',
+            compositionTabName: 'Pool Composition v2',
+            emissionDataTabName: 'EmissionData',
+        },
+        canary: {
+            user: 'datafeed-service@datastudio-366113.iam.gserviceaccount.com',
+            sheetId: '1HnJOuRQXGy06tNgqjYMzQNIsaCSCC01Yxe_lZhXBDpY',
+            databaseTabName: 'Database v2',
+            compositionTabName: 'Pool Composition v2',
+            emissionDataTabName: 'EmissionData',
+        },
+    },
+    monitoring: {
+        main: {
+            alarmTopicArn: 'arn:aws:sns:ca-central-1:118697801881:api_alarms',
+        },
+        canary: {
+            alarmTopicArn: 'arn:aws:sns:eu-central-1:118697801881:api_alarms',
+        },
+    },
+};
+
+export const gnosisNetworkConfig: NetworkConfig = {
+    data: gnosisNetworkData,
+    contentService: new GithubContentService(),
+    provider: new ethers.providers.JsonRpcProvider(gnosisNetworkData.rpcUrl),
+    poolAprServices: [
+        new PhantomStableAprService(gnosisNetworkData.balancer.yieldProtocolFeePercentage),
+        new BoostedPoolAprService(gnosisNetworkData.balancer.yieldProtocolFeePercentage),
+        new SwapFeeAprService(gnosisNetworkData.balancer.swapProtocolFeePercentage),
+        new GaugeAprService(gaugeSubgraphService, tokenService, [
+            gnosisNetworkData.beets.address,
+            gnosisNetworkData.bal.address,
+        ]),
+    ],
+    poolStakingServices: [new GaugeStakingService(gaugeSubgraphService)],
+    tokenPriceHandlers: [
+        new BptPriceHandlerService(),
+        new LinearWrappedTokenPriceHandlerService(),
+        new SwapsPriceHandlerService(),
+    ],
+    userStakedBalanceServices: [new UserSyncGaugeBalanceService()],
+    /*
+    For sub-minute jobs we set the alarmEvaluationPeriod and alarmDatapointsToAlarm to 1 instead of the default 3. 
+    This is needed because the minimum alarm period is 1 minute and we want the alarm to trigger already after 1 minute instead of 3.
+
+    For every 1 days jobs we set the alarmEvaluationPeriod and alarmDatapointsToAlarm to 1 instead of the default 3. 
+    This is needed because the maximum alarm evaluation period is 1 day (period * evaluationPeriod).
+    */
+    workerJobs: [
+        {
+            name: 'update-token-prices',
+            interval: every(2, 'minutes'),
+        },
+        {
+            name: 'update-liquidity-for-inactive-pools',
+            interval: every(1, 'days'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'update-liquidity-for-active-pools',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'update-pool-apr',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'load-on-chain-data-for-pools-with-active-updates',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'sync-new-pools-from-subgraph',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'sync-tokens-from-pool-tokens',
+            interval: every(5, 'minutes'),
+        },
+        {
+            name: 'update-liquidity-24h-ago-for-all-pools',
+            interval: every(5, 'minutes'),
+        },
+        {
+            name: 'cache-average-block-time',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'sync-staking-for-pools',
+            interval: every(5, 'minutes'),
+        },
+        {
+            name: 'sync-latest-snapshots-for-all-pools',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'update-lifetime-values-for-all-pools',
+            interval: every(30, 'minutes'),
+        },
+        {
+            name: 'sync-changed-pools',
+            interval: every(15, 'seconds'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'user-sync-wallet-balances-for-all-pools',
+            interval: every(10, 'seconds'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'user-sync-staked-balances',
+            interval: every(10, 'seconds'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'sync-user-snapshots',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'sync-coingecko-coinids',
+            interval: every(2, 'hours'),
+        },
+        {
+            name: 'purge-old-tokenprices',
+            interval: every(1, 'days'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+    ],
+};
