c8cb3c0f35e8053c2cea33f6e7ffc2583190ee0b
franz <franzns11@gmail.com>
2023-06-01 09:27:05 +0200

    add comment

--- modules/user/lib/user-sync-wallet-balance.service.ts
+++ modules/user/lib/user-sync-wallet-balance.service.ts
@@ -96,6 +96,7 @@ export class UserSyncWalletBalanceService {
         }
 
         const fromBlock = syncStatus.blockNumber + 1;
+        // as we use the erc20 transfer topic, we use a smaller block range than defined in the network context
         const toBlock = latestBlock - fromBlock > 200 ? fromBlock + 200 : latestBlock;
 
         // no new blocks have been minted, needed for slow networks
