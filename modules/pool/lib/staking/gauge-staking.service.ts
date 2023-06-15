0b8a87c0312d6cb9f1cfb2d67fc9fdc890726e6c
franz <franzns11@gmail.com>
2023-06-15 19:50:23 +0200

    get reward rate on chain

--- modules/pool/lib/staking/gauge-staking.service.ts
+++ modules/pool/lib/staking/gauge-staking.service.ts
@@ -4,11 +4,14 @@ import { prismaBulkExecuteOperations } from '../../../../prisma/prisma-util';
 import { PrismaPoolStakingType } from '@prisma/client';
 import { networkContext } from '../../../network/network-context.service';
 import { GaugeSubgraphService, LiquidityGaugeStatus } from '../../../subgraphs/gauge-subgraph/gauge-subgraph.service';
-import { Interface, formatEther } from 'ethers/lib/utils';
+import { Interface, formatEther, formatUnits } from 'ethers/lib/utils';
 import { getContractAt } from '../../../web3/contract';
-import childChainGaugeAbi from './abi/ChildChainGauge.json';
+import childChainGaugeV2Abi from './abi/ChildChainGaugeV2.json';
+import childChainGaugeV1Abi from './abi/ChildChainGaugeV1.json';
 import multicall3Abi from './abi/Multicall3.json';
 import moment from 'moment';
+import { formatFixed } from '@ethersproject/bignumber';
+import { BigNumber } from 'ethers';
 
 interface ChildChainInfo {
     /** 1 for old gauges, 2 for gauges receiving cross chain BAL rewards */
@@ -62,6 +65,8 @@ export class GaugeStakingService implements PoolStakingService {
                         }),
                     );
 
+                    const gaugeVersion = childChainGaugeInfo[gauge.id] ? childChainGaugeInfo[gauge.id].version : 1;
+
                     operations.push(
                         prisma.prismaPoolStakingGauge.upsert({
                             where: { id_chain: { id: gauge.id, chain: networkContext.chain } },
@@ -71,7 +76,7 @@ export class GaugeStakingService implements PoolStakingService {
                                 gaugeAddress: gauge.id,
                                 chain: networkContext.chain,
                                 status: gaugeStatus,
-                                version: childChainGaugeInfo[gauge.id] ? childChainGaugeInfo[gauge.id].version : 1,
+                                version: gaugeVersion,
                             },
                             update: {
                                 status: gaugeStatus,
@@ -81,7 +86,7 @@ export class GaugeStakingService implements PoolStakingService {
 
                     // Add BAL as a reward token for the v2 gauge
                     // need to add '-0' to the ID because it get's split by that further down.
-                    if (childChainGaugeInfo[gauge.id].version === 2) {
+                    if (gaugeVersion === 2) {
                         if (gauge.tokens) {
                             gauge.tokens.push({
                                 id: `${networkContext.data.bal.address}-0`,
@@ -105,10 +110,31 @@ export class GaugeStakingService implements PoolStakingService {
                             const tokenAddress = rewardToken.id.split('-')[0].toLowerCase();
                             const id = `${gauge.id}-${tokenAddress}`;
 
-                            // the rate of the token is still set although period is finished, we reset it here
-                            if (rewardToken.periodFinish) {
-                                if (parseFloat(rewardToken.periodFinish) < moment().unix()) {
-                                    rewardToken.rate = '0';
+                            let rewardRate = '0.0';
+                            let periodFinish: number;
+
+                            if (gaugeVersion === 1) {
+                                const gaugeV1 = await getContractAt(gauge.id, childChainGaugeV1Abi);
+                                const rewardData = await gaugeV1.reward_data(tokenAddress);
+
+                                periodFinish = rewardData[2];
+                                if (periodFinish > moment().unix()) {
+                                    // period still running
+                                    rewardRate = formatFixed(rewardData[3], 18);
+                                }
+                            } else {
+                                // we can't get BAL rate from the reward data but got it from the inflation_rate call which set the rewardToken.rate
+                                if (tokenAddress === networkContext.data.bal.address) {
+                                    rewardRate = rewardToken.rate ? rewardToken.rate : '0.0';
+                                } else {
+                                    const gaugeV2 = await getContractAt(gauge.id, childChainGaugeV2Abi);
+                                    const rewardData = await gaugeV2.reward_data(tokenAddress);
+
+                                    periodFinish = parseFloat(formatUnits(rewardData[1], 0));
+                                    if (periodFinish > moment().unix()) {
+                                        // period still running
+                                        rewardRate = formatFixed(rewardData[2], 18);
+                                    }
                                 }
                             }
 
@@ -119,10 +145,10 @@ export class GaugeStakingService implements PoolStakingService {
                                         chain: networkContext.chain,
                                         gaugeId: gauge.id,
                                         tokenAddress: tokenAddress,
-                                        rewardPerSecond: `${rewardToken.rate}`,
+                                        rewardPerSecond: rewardRate,
                                     },
                                     update: {
-                                        rewardPerSecond: `${rewardToken.rate}`,
+                                        rewardPerSecond: rewardRate,
                                     },
                                     where: { id_chain: { id, chain: networkContext.chain } },
                                 }),
@@ -137,7 +163,7 @@ export class GaugeStakingService implements PoolStakingService {
     }
 
     async getChildChainGaugeInfo(gaugeAddresses: string[]): Promise<{ [gaugeAddress: string]: ChildChainInfo }> {
-        const iChildChainGauge = new Interface(childChainGaugeAbi);
+        const iChildChainGauge = new Interface(childChainGaugeV2Abi);
         const multicall = getContractAt(networkContext.data.multicall3, multicall3Abi);
 
         const currentWeek = Math.floor(Date.now() / 1000 / 604800);
