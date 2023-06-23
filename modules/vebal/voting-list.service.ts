6cf2ce1bfdc151471c7a81ab4c91ec028a8c2564
Alberto Gualis <agualis@gmail.com>
2023-06-23 10:47:48 +0200

    Clean voting-list-service

--- modules/vebal/voting-list.service.ts
+++ modules/vebal/voting-list.service.ts
@@ -1,47 +1,12 @@
-import { formatUnits, getAddress } from 'ethers/lib/utils';
-import { capitalize, flatten, chain, times, chunk, zipObject } from 'lodash';
-import { setRequestScopedContextValue } from '../../modules/context/request-scoped-context';
-import { AllNetworkConfigs } from '../../modules/network/network-config';
+import { formatFixed } from '@ethersproject/bignumber';
+import { formatUnits } from 'ethers/lib/utils';
+import { chunk, zipObject } from 'lodash';
 import { networkContext } from '../../modules/network/network-context.service';
-import {
-    LiquidityGaugesInfo,
-    gaugeSubgraphService,
-} from '../../modules/subgraphs/gauge-subgraph/gauge-subgraph.service';
 import { getContractAt } from '../../modules/web3/contract';
-import { NetworkConfig } from '../network/network-config-types';
 import { prisma } from '../../prisma/prisma-client';
-import { Chain as PrismaChain, PrismaPoolStakingGauge } from '@prisma/client';
-import { Chain as SubgraphChain } from '../subgraphs/gauge-subgraph/generated/gauge-subgraph-types';
 import { Multicaller } from '../web3/multicaller';
 import gaugeControllerAbi from './abi/gaugeController.json';
-import { formatFixed } from '@ethersproject/bignumber';
-
-type LiquidityGaugeInfo = LiquidityGaugesInfo[number];
-interface GaugeInfo extends LiquidityGaugeInfo {
-    chain: PrismaChain;
-}
-type GaugesInfo = GaugeInfo[];
-
 export class VotingListService {
-    async syncVotingList() {
-        const promises = Object.values(AllNetworkConfigs).map(async (network) => fetchGaugesInfo(network));
-        const gaugeInfoForAllNetworks = await Promise.all(promises);
-        const gaugesInfo = flatten(gaugeInfoForAllNetworks);
-
-        console.log('Total gauges', gaugesInfo.length);
-
-        const validGauges = await filterValidGauges(gaugesInfo);
-
-        console.log('Total valid gauges', validGauges.length);
-
-        // console.log('First gauge record: ', validGauges[0]);
-        return validGauges;
-    }
-
-    async getList(): Promise<PrismaPoolStakingGauge[]> {
-        return await prisma.prismaPoolStakingGauge.findMany();
-    }
-
     /**
      * This query illustrates the pool related data that we need for each gauge
      * Ideally, we don't want to denormalize but how can we join validGauges with getPoolsForVotingList in an efficient way?
@@ -83,66 +48,6 @@ export class VotingListService {
 }
 
 export const votingListService = new VotingListService();
-
-async function fetchGaugesInfo(network: NetworkConfig): Promise<GaugesInfo> {
-    const chainId = network.data.chain.id;
-    setRequestScopedContextValue('chainId', chainId.toString());
-
-    const isMainnet = chainId === 1;
-    //TODO: We only have poolsWithGauges for mainnet and arbitrum for testing purposes
-    if (![1, 42161].includes(chainId)) return [];
-    // if (![42161].includes(chainId)) return [];
-    const poolIds = network.poolsWithGauges as string[];
-
-    if (isMainnet) return fetchMainnetGaugesInfo(poolIds);
-    return fetchL2GaugesInfo(poolIds, network);
-}
-
-async function fetchMainnetGaugesInfo(poolIds: string[]): Promise<GaugesInfo> {
-    const gaugesInfo = await gaugeSubgraphService.getGaugesInfo(poolIds);
-    console.log(`⬇️  Fetched ${gaugesInfo.length} gauge info records for Mainnet`);
-    return gaugesInfo.map((rootGauge) => {
-        return { ...rootGauge, chain: 'MAINNET' };
-    });
-}
-
-async function fetchL2GaugesInfo(poolIds: string[], network: NetworkConfig): Promise<GaugesInfo> {
-    const streamers = (await gaugeSubgraphService.getGaugesStreamers(poolIds)).map((gauge) => {
-        return {
-            address: gauge.id,
-            streamer: gauge.streamer,
-            poolId: gauge.poolId as string,
-        };
-    });
-
-    const poolIdsByRecipient = streamers.reduce((acc: Record<string, string>, streamer) => {
-        const recipient = streamer.streamer ? streamer.streamer : streamer.address;
-        acc[recipient] = streamer.poolId;
-        return acc;
-    }, {});
-
-    // TODO: improve TS check
-    const chainName: SubgraphChain = capitalize(network.data.chain.slug) as SubgraphChain;
-
-    const recipients = Object.keys(poolIdsByRecipient);
-    let rootGauges = await gaugeSubgraphService.getRootGaugesInfo(recipients, chainName);
-    console.log(`⬇️  Fetched ${rootGauges.length} gauge info records for ${chainName}`);
-
-    return rootGauges.map((rootGauge) => {
-        // Manually add poolId as we cannot get it in the RootGauges query above
-        return { ...rootGauge, poolId: poolIdsByRecipient[rootGauge.recipient], chain: network.data.chain.prismaId };
-    });
-}
-
-/**
- * Excludes killed gauges with zero weight
- */
-async function filterValidGauges(gauges: GaugesInfo): Promise<GaugesInfo> {
-    const relativeWeights = await fetchGaugeRelativeWeights();
-
-    const validGauges = gauges.filter(({ id, isKilled }) => !isKilled || relativeWeights[getAddress(id)] !== '0.0');
-    return validGauges;
-}
 
 /**
  * Fetches the relative weight of all gauges using the GaugeController Contract following this steps:
