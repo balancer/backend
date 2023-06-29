ebe07244b9a34800ee19926da1e52b8296757d17
Alberto Gualis <agualis@gmail.com>
2023-06-29 18:39:10 +0200

    Tests using public rpc url

--- modules/vebal/voting-list.service.spec.ts
+++ modules/vebal/voting-list.service.spec.ts
@@ -4,17 +4,21 @@ import { VotingListService } from './voting-list.service';
 // anvil --fork-url https://eth-mainnet.alchemyapi.io/v2/7gYoDJEw6-QyVP5hd2UfZyelzDIDemGz --port 8555 --fork-block-number=17569375
 
 it('fetches list of root gauge addresses', async () => {
-    const service = new VotingListService(testPublicClient());
+    const service = new VotingListService(testPublicClient('https://cloudflare-eth.com'));
     const addresses = await service.getRootGaugeAddresses();
     expect(addresses.length).toBe(327);
 }, 10_000);
 
 it('generates root gauge rows given a list of gauge addresses', async () => {
-    const service = new VotingListService(testPublicClient());
+    const service = new VotingListService(testPublicClient('https://cloudflare-eth.com'));
+
     const rootGaugeAddresses = [
         '0x79eF6103A513951a3b25743DB509E267685726B7',
         '0xfb0265841C49A6b19D70055E596b212B0dA3f606',
     ];
+    // Uncomment to test with all addresses
+    // const rootGaugeAddresses = await service.getRootGaugeAddresses();
+
     const rows = await service.generateRootGaugeRows(rootGaugeAddresses);
 
     expect(rows).toMatchInlineSnapshot(`
