80d8e4d901985e90aa8fe45adb1eb6841c0b0b9d
Alberto Gualis <agualis@gmail.com>
2023-07-25 18:10:00 +0200

    Add comment for ignored Cron gauge

--- modules/vebal/special-pools/special-root-gauge-addresses.ts
+++ modules/vebal/special-pools/special-root-gauge-addresses.ts
@@ -14,13 +14,11 @@ export const specialRootGaugeAddresses = [
     ...veGauges,
     ...hardCodedPools.map((pool) => pool.rootGauge.address),
 
-    // Balancer USDC/WETH/L Gauge Deposit
+    // Balancer USDC/WETH/L Gauge from Cron Finance
     // https://etherscan.io/address/0xc4e72abe8a32fd7d7ba787e1ec860ecb8c0b333c#readContract
-    // Valid root gauge without Staking relation (MAINNET??)
+    // We exclude this gauge because Cron Contract is not compliant with BasePool interface so we won't find it the pools from the Subgraph
     '0xc4e72abe8a32fd7d7ba787e1ec860ecb8c0b333c',
 
     // ARBITRUM Killed root gauge that shares staking with '0xd758454bdf4df7ad85f7538dc9742648ef8e6d0a' (was failing due to unique constraint)
     '0x3f829a8303455cb36b7bcf3d1bdc18d5f6946aea',
-
-    // '0xf0d887c1f5996c91402eb69ab525f028dd5d7578',
 ];
