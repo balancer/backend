fa9601162d1485dd1d5ec3c69a81aef646f5f4b0
Alberto Gualis <agualis@gmail.com>
2023-07-26 17:29:20 +0200

    Improve excluded Cron gauge comment

--- modules/vebal/special-pools/special-root-gauge-addresses.ts
+++ modules/vebal/special-pools/special-root-gauge-addresses.ts
@@ -14,8 +14,10 @@ export const specialRootGaugeAddresses = [
     ...veGauges,
     ...hardCodedPools.map((pool) => pool.rootGauge.address),
 
-    // Balancer USDC/WETH/L Gauge from Cron Finance
-    // https://etherscan.io/address/0xc4e72abe8a32fd7d7ba787e1ec860ecb8c0b333c#readContract
-    // We exclude this gauge because Cron Contract is not compliant with BasePool interface so we won't find it the pools from the Subgraph
+    /* Balancer USDC/WETH/L Gauge from Cron Finance
+     https://etherscan.io/address/0xc4e72abe8a32fd7d7ba787e1ec860ecb8c0b333c#readContract
+     This is a special gauge because Cron Contract is not compliant with BasePool interface so we won't find it the pools from the Subgraph
+     Adding it to the voting list would require a hardcoded pool but we won't do it for now unless some stakeholder explicitly asks for it
+    */
     '0xc4e72abe8a32fd7d7ba787e1ec860ecb8c0b333c',
 ];
