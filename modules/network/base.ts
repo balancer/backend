0d3becffdd93c9fcc3088c65613c89a1bf4848a8
Tim Robinson <tim@timjrobinson.com>
2023-07-27 18:58:24 +1000

    Add base config

---
+++ modules/network/base.ts
@@ -0,0 +1,275 @@
+import { BigNumber, ethers } from 'ethers';
+import { NetworkConfig, NetworkData } from './network-config-types';
+import { tokenService } from '../token/token.service';
+import { WstethAprService } from '../pool/lib/apr-data-sources/optimism/wsteth-apr.service';
+import { ReaperCryptAprService } from '../pool/lib/apr-data-sources/reaper-crypt-apr.service';
+import { PhantomStableAprService } from '../pool/lib/apr-data-sources/phantom-stable-apr.service';
+import { BoostedPoolAprService } from '../pool/lib/apr-data-sources/boosted-pool-apr.service';
+import { SwapFeeAprService } from '../pool/lib/apr-data-sources/swap-fee-apr.service';
+import { GaugeAprService } from '../pool/lib/apr-data-sources/ve-bal-gauge-apr.service';
+import { GaugeStakingService } from '../pool/lib/staking/gauge-staking.service';
+import { BptPriceHandlerService } from '../token/lib/token-price-handlers/bpt-price-handler.service';
+import { LinearWrappedTokenPriceHandlerService } from '../token/lib/token-price-handlers/linear-wrapped-token-price-handler.service';
+import { SwapsPriceHandlerService } from '../token/lib/token-price-handlers/swaps-price-handler.service';
+import { UserSyncGaugeBalanceService } from '../user/lib/user-sync-gauge-balance.service';
+import { every } from '../../worker/intervals';
+import { GithubContentService } from '../content/github-content.service';
+import { gaugeSubgraphService } from '../subgraphs/gauge-subgraph/gauge-subgraph.service';
+import { CoingeckoPriceHandlerService } from '../token/lib/token-price-handlers/coingecko-price-handler.service';
+import { coingeckoService } from '../coingecko/coingecko.service';
+
+const baseNetworkData: NetworkData = {
+    chain: {
+        slug: 'base',
+        id: 8453,
+        nativeAssetAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+        wrappedNativeAssetAddress: '0x4200000000000000000000000000000000000006',
+        prismaId: 'BASE',
+        gqlId: 'BASE',
+    },
+    subgraphs: {
+        startDate: '2023-07-10',
+        balancer: 'https://api.studio.thegraph.com/query/24660/balancer-base-v2/version/latest',
+        beetsBar: '',
+        blocks: '',
+        gauge: '',
+        veBalLocks: '',
+        userBalances: '',
+    },
+    eth: {
+        address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        addressFormatted: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+        symbol: 'ETH',
+        name: 'Ether',
+    },
+    weth: {
+        address: '0x4200000000000000000000000000000000000006',
+        addressFormatted: '0x4200000000000000000000000000000000000006',
+    },
+    coingecko: {
+        nativeAssetId: 'ethereum',
+        platformId: 'base',
+        excludedTokenAddresses: [],
+    },
+    tokenPrices: {
+        maxHourlyPriceHistoryNumDays: 100,
+    },
+    rpcUrl: 'https://developer-access-mainnet.base.org',
+    rpcMaxBlockRange: 2000,
+    beetsPriceProviderRpcUrl: '',
+    sanity: {
+        projectId: '',
+        dataset: '',
+    },
+    protocolToken: 'bal',
+    beets: {
+        address: '0x0000000000000000000000000000000000000000',
+    },
+    bal: {
+        address: '0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2',
+    },
+    veBal: {
+        address: '',
+        delegationProxy: '',
+    },
+    balancer: {
+        vault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+        composableStablePoolFactories: [
+            '0x8df317a729fcaA260306d7de28888932cb579b88',
+        ],
+        weightedPoolV2Factories: [
+            '0x4C32a8a8fDa4E24139B51b456B42290f51d6A1c4',
+        ],
+        swapProtocolFeePercentage: 0.5,
+        yieldProtocolFeePercentage: 0.5,
+        poolDataQueryContract: '',
+    },
+    multicall: '0xca11bde05977b3631167028862be2a173976ca11',
+    multicall3: '0xca11bde05977b3631167028862be2a173976ca11',
+    masterchef: {
+        address: '0x0000000000000000000000000000000000000000',
+        excludedFarmIds: [],
+    },
+    avgBlockSpeed: 2,
+    sor: {
+        main: {
+            url: 'https://uu6cfghhd5lqa7py3nojxkivd40zuugb.lambda-url.ca-central-1.on.aws/',
+            maxPools: 8,
+            forceRefresh: false,
+            gasPrice: BigNumber.from(10),
+            swapGas: BigNumber.from('1000000'),
+        },
+        canary: {
+            url: 'https://ksa66wlkjbvteijxmflqjehsay0jmekw.lambda-url.eu-central-1.on.aws/',
+            maxPools: 8,
+            forceRefresh: false,
+            gasPrice: BigNumber.from(10),
+            swapGas: BigNumber.from('1000000'),
+        },
+    },
+    yearn: {
+        vaultsEndpoint: 'https://#/',
+    },
+    reaper: {
+        linearPoolFactories: [],
+        linearPoolIdsFromErc4626Factory: [],
+        averageAPRAcrossLastNHarvests: 2,
+    },
+    beefy: {
+        linearPools: [''],
+    },
+    lido: {
+        wstEthAprEndpoint: 'https://eth-api.lido.fi/v1/protocol/steth/apr/sma',
+        wstEthContract: '',
+    },
+    datastudio: {
+        main: {
+            user: 'datafeed-service@datastudio-366113.iam.gserviceaccount.com',
+            sheetId: '11anHUEb9snGwvB-errb5HvO8TvoLTRJhkDdD80Gxw1Q',
+            databaseTabName: 'Database v2',
+            compositionTabName: 'Pool Composition v2',
+            emissionDataTabName: 'EmissionData',
+        },
+        canary: {
+            user: 'datafeed-service@datastudio-366113.iam.gserviceaccount.com',
+            sheetId: '1HnJOuRQXGy06tNgqjYMzQNIsaCSCC01Yxe_lZhXBDpY',
+            databaseTabName: 'Database v2',
+            compositionTabName: 'Pool Composition v2',
+            emissionDataTabName: 'EmissionData',
+        },
+    },
+    monitoring: {
+        main: {
+            alarmTopicArn: 'arn:aws:sns:ca-central-1:118697801881:api_alarms',
+        },
+        canary: {
+            alarmTopicArn: 'arn:aws:sns:eu-central-1:118697801881:api_alarms',
+        },
+    },
+};
+
+export const baseNetworkConfig: NetworkConfig = {
+    data: baseNetworkData,
+    contentService: new GithubContentService(),
+    provider: new ethers.providers.JsonRpcProvider(baseNetworkData.rpcUrl),
+    poolAprServices: [
+        new WstethAprService(tokenService, baseNetworkData.lido!.wstEthContract),
+        new BoostedPoolAprService(),
+        new SwapFeeAprService(baseNetworkData.balancer.swapProtocolFeePercentage),
+        new GaugeAprService(gaugeSubgraphService, tokenService, [
+            baseNetworkData.beets.address,
+            baseNetworkData.bal.address,
+        ]),
+    ],
+    poolStakingServices: [new GaugeStakingService(gaugeSubgraphService)],
+    tokenPriceHandlers: [
+        new CoingeckoPriceHandlerService(coingeckoService),
+        new BptPriceHandlerService(),
+        new LinearWrappedTokenPriceHandlerService(),
+        new SwapsPriceHandlerService(),
+    ],
+    userStakedBalanceServices: [new UserSyncGaugeBalanceService()],
+    /*
+    For sub-minute jobs we set the alarmEvaluationPeriod and alarmDatapointsToAlarm to 1 instead of the default 3. 
+    This is needed because the minimum alarm period is 1 minute and we want the alarm to trigger already after 1 minute instead of 3.
+
+    For every 1 days jobs we set the alarmEvaluationPeriod and alarmDatapointsToAlarm to 1 instead of the default 3. 
+    This is needed because the maximum alarm evaluation period is 1 day (period * evaluationPeriod).
+    */
+    workerJobs: [
+        {
+            name: 'update-token-prices',
+            interval: every(2, 'minutes'),
+        },
+        {
+            name: 'update-liquidity-for-inactive-pools',
+            interval: every(1, 'days'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'update-liquidity-for-active-pools',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'update-pool-apr',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'load-on-chain-data-for-pools-with-active-updates',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'sync-new-pools-from-subgraph',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'sync-tokens-from-pool-tokens',
+            interval: every(5, 'minutes'),
+        },
+        {
+            name: 'update-liquidity-24h-ago-for-all-pools',
+            interval: every(5, 'minutes'),
+        },
+        {
+            name: 'cache-average-block-time',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'sync-staking-for-pools',
+            interval: every(5, 'minutes'),
+        },
+        {
+            name: 'sync-latest-snapshots-for-all-pools',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'update-lifetime-values-for-all-pools',
+            interval: every(30, 'minutes'),
+        },
+        {
+            name: 'sync-changed-pools',
+            interval: every(15, 'seconds'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'user-sync-wallet-balances-for-all-pools',
+            interval: every(10, 'seconds'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'user-sync-staked-balances',
+            interval: every(10, 'seconds'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'sync-user-snapshots',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'sync-coingecko-coinids',
+            interval: every(2, 'hours'),
+        },
+        {
+            name: 'purge-old-tokenprices',
+            interval: every(1, 'days'),
+            alarmEvaluationPeriod: 1,
+            alarmDatapointsToAlarm: 1,
+        },
+        {
+            name: 'update-fee-volume-yield-all-pools',
+            interval: every(1, 'hours'),
+        },
+        {
+            name: 'sync-vebal-balances',
+            interval: every(1, 'minutes'),
+        },
+        {
+            name: 'sync-vebal-totalSupply',
+            interval: every(5, 'minutes'),
+        },
+    ],
+};
