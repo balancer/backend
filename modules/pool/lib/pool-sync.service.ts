74daf91b34fa39089287fcc8c8ccb19a2cf2e277
franz <franzns11@gmail.com>
2023-08-07 09:21:25 +0200

    add init for on chain data

--- modules/pool/lib/pool-sync.service.ts
+++ modules/pool/lib/pool-sync.service.ts
@@ -44,7 +44,7 @@ export class PoolSyncService {
         );
         const poolIds: string[] = _.uniq(filteredEvents.map((event) => event.args!.poolId));
         if (poolIds.length !== 0) {
-            console.log(`Syncing ${poolIds.length} pools`);
+            console.log(`Syncing ${poolIds.length} pools between blocks ${startBlock} and ${endBlock}`);
             await poolService.updateOnChainDataForPools(poolIds, endBlock);
 
             const poolsWithNewSwaps = await poolService.syncSwapsForLast48Hours();
@@ -59,6 +59,35 @@ export class PoolSyncService {
             create: {
                 category: PrismaLastBlockSyncedCategory.POOLS,
                 blockNumber: endBlock,
+                chain: networkContext.chain,
+            },
+        });
+    }
+
+    public async initOnChainDataForAllPools() {
+        const latestBlock = await networkContext.provider.getBlockNumber();
+
+        const allPools = await prisma.prismaPool.findMany({
+            where: { chain: networkContext.chain },
+        });
+
+        const poolIds = allPools.map((pool) => pool.id);
+
+        await poolService.updateOnChainStatusForPools(poolIds);
+
+        console.log(`Init syncing ${poolIds.length} pools up to block ${latestBlock}`);
+        await poolService.updateOnChainDataForPools(poolIds, latestBlock);
+        await poolService.syncSwapsForLast48Hours();
+        await poolService.updateVolumeAndFeeValuesForPools(poolIds);
+
+        await prisma.prismaLastBlockSynced.upsert({
+            where: { category_chain: { category: PrismaLastBlockSyncedCategory.POOLS, chain: networkContext.chain } },
+            update: {
+                blockNumber: latestBlock,
+            },
+            create: {
+                category: PrismaLastBlockSyncedCategory.POOLS,
+                blockNumber: latestBlock,
                 chain: networkContext.chain,
             },
         });
