04f62448d725324b4f75982b15be1e93fcdb0bfd
franz <franzns11@gmail.com>
2023-11-08 18:00:04 +0100

    change rpc

--- modules/network/fantom.ts
+++ modules/network/fantom.ts
@@ -93,7 +93,7 @@ const fantomNetworkData: NetworkData = {
     tokenPrices: {
         maxHourlyPriceHistoryNumDays: 100,
     },
-    rpcUrl: 'https://rpc.fantom.gateway.fm',
+    rpcUrl: 'https://ftm.obsidianrpc.com',
     rpcMaxBlockRange: 1000,
     sanity: {
         projectId: '1g2ag2hb',
