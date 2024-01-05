a567a42d81eb597560f918dbade398386b6f84b1
franz <franzns11@gmail.com>
2024-01-05 11:05:06 +0100

    optimize lifetimevalue update

--- modules/pool/lib/pool-usd-data.service.ts
+++ modules/pool/lib/pool-usd-data.service.ts
@@ -279,66 +279,103 @@ export class PoolUsdDataService {
             where: { chain: this.chain, balanceNum: { gt: 0 } },
         });
 
+        const subgraphPoolIds = subgraphPools.map((pool) => pool.id);
+
+        const dbPools = await prisma.prismaPool.findMany({
+            where: { id: { in: subgraphPoolIds }, chain: this.chain },
+            include: {
+                dynamicData: true,
+                snapshots: true,
+            },
+        });
+
         for (const pool of subgraphPools) {
             const staked = stakedUsers.find((stakedUser) => stakedUser.poolId === pool.id);
+            const dbPool = dbPools.find((poolInDb) => poolInDb.id === pool.id);
+            if (!dbPool) continue;
 
             // Limit numbers, as we have seen some pools with skewd values
             const max = 1e18;
             const lifetimeVolume = Math.min(parseFloat(pool.totalSwapVolume), max);
             const lifetimeSwapFees = Math.min(parseFloat(pool.totalSwapFee), max);
+            const holdersCount = parseInt(pool.holdersCount) + (staked?._count.userAddress || 0);
 
-            updates.push(
-                prisma.prismaPoolDynamicData.update({
-                    where: { id_chain: { id: pool.id, chain: this.chain } },
-                    data: {
-                        lifetimeVolume: lifetimeVolume,
-                        lifetimeSwapFees: lifetimeSwapFees,
-                        holdersCount: parseInt(pool.holdersCount) + (staked?._count.userAddress || 0),
-                        swapsCount: parseInt(pool.swapsCount),
-                    },
-                }),
-            );
-
-            const snapshots = await prisma.prismaPoolSnapshot.findMany({
-                where: { poolId: pool.id, chain: this.chain },
-            });
-
-            if (snapshots.length > 0) {
-                const sharePriceAth = _.orderBy(snapshots, 'sharePrice', 'desc')[0];
-                const sharePriceAtl = _.orderBy(snapshots, 'sharePrice', 'asc')[0];
-                const totalLiquidityAth = _.orderBy(snapshots, 'totalLiquidity', 'desc')[0];
-                const totalLiquidityAtl = _.orderBy(snapshots, 'totalLiquidity', 'asc')[0];
-                const volume24hAth = _.orderBy(snapshots, 'volume24h', 'desc')[0];
-                const volume24hAtl = _.orderBy(snapshots, 'volume24h', 'asc')[0];
-                const fees24hAth = _.orderBy(snapshots, 'fees24h', 'desc')[0];
-                const fees24hAtl = _.orderBy(snapshots, 'fees24h', 'asc')[0];
-
+            if (
+                !dbPool.dynamicData ||
+                dbPool.dynamicData.lifetimeVolume !== lifetimeVolume ||
+                dbPool.dynamicData.lifetimeSwapFees !== lifetimeSwapFees ||
+                dbPool.dynamicData.holdersCount !== holdersCount ||
+                dbPool.dynamicData.swapsCount !== parseInt(pool.swapsCount)
+            ) {
                 updates.push(
                     prisma.prismaPoolDynamicData.update({
                         where: { id_chain: { id: pool.id, chain: this.chain } },
                         data: {
-                            sharePriceAth: sharePriceAth.sharePrice,
-                            sharePriceAthTimestamp: sharePriceAth.timestamp,
-                            sharePriceAtl: sharePriceAtl.sharePrice,
-                            sharePriceAtlTimestamp: sharePriceAtl.timestamp,
-
-                            totalLiquidityAth: totalLiquidityAth.totalLiquidity,
-                            totalLiquidityAthTimestamp: totalLiquidityAth.timestamp,
-                            totalLiquidityAtl: totalLiquidityAtl.totalLiquidity,
-                            totalLiquidityAtlTimestamp: totalLiquidityAtl.timestamp,
-
-                            volume24hAth: volume24hAth.volume24h,
-                            volume24hAthTimestamp: volume24hAth.timestamp,
-                            volume24hAtl: volume24hAtl.volume24h,
-                            volume24hAtlTimestamp: volume24hAtl.timestamp,
-
-                            fees24hAth: fees24hAth.fees24h,
-                            fees24hAthTimestamp: fees24hAth.timestamp,
-                            fees24hAtl: fees24hAtl.fees24h,
-                            fees24hAtlTimestamp: fees24hAtl.timestamp,
+                            lifetimeVolume: lifetimeVolume,
+                            lifetimeSwapFees: lifetimeSwapFees,
+                            holdersCount: holdersCount,
+                            swapsCount: parseInt(pool.swapsCount),
                         },
                     }),
                 );
+            }
+
+            if (dbPool.snapshots.length > 0) {
+                const sharePriceAth = _.orderBy(dbPool.snapshots, 'sharePrice', 'desc')[0];
+                const sharePriceAtl = _.orderBy(dbPool.snapshots, 'sharePrice', 'asc')[0];
+                const totalLiquidityAth = _.orderBy(dbPool.snapshots, 'totalLiquidity', 'desc')[0];
+                const totalLiquidityAtl = _.orderBy(dbPool.snapshots, 'totalLiquidity', 'asc')[0];
+                const volume24hAth = _.orderBy(dbPool.snapshots, 'volume24h', 'desc')[0];
+                const volume24hAtl = _.orderBy(dbPool.snapshots, 'volume24h', 'asc')[0];
+                const fees24hAth = _.orderBy(dbPool.snapshots, 'fees24h', 'desc')[0];
+                const fees24hAtl = _.orderBy(dbPool.snapshots, 'fees24h', 'asc')[0];
+
+                if (
+                    !dbPool.dynamicData ||
+                    dbPool.dynamicData.sharePriceAth !== sharePriceAth.sharePrice ||
+                    dbPool.dynamicData.sharePriceAthTimestamp !== sharePriceAth.timestamp ||
+                    dbPool.dynamicData.sharePriceAtl !== sharePriceAtl.sharePrice ||
+                    dbPool.dynamicData.sharePriceAtlTimestamp !== sharePriceAtl.timestamp ||
+                    dbPool.dynamicData.totalLiquidityAth !== totalLiquidityAth.totalLiquidity ||
+                    dbPool.dynamicData.totalLiquidityAthTimestamp !== totalLiquidityAth.timestamp ||
+                    dbPool.dynamicData.totalLiquidityAtl !== totalLiquidityAtl.totalLiquidity ||
+                    dbPool.dynamicData.totalLiquidityAtlTimestamp !== totalLiquidityAtl.timestamp ||
+                    dbPool.dynamicData.volume24hAth !== volume24hAth.volume24h ||
+                    dbPool.dynamicData.volume24hAthTimestamp !== volume24hAth.timestamp ||
+                    dbPool.dynamicData.volume24hAtl !== volume24hAtl.volume24h ||
+                    dbPool.dynamicData.volume24hAtlTimestamp !== volume24hAtl.timestamp ||
+                    dbPool.dynamicData.fees24hAth !== fees24hAth.fees24h ||
+                    dbPool.dynamicData.fees24hAthTimestamp !== fees24hAth.timestamp ||
+                    dbPool.dynamicData.fees24hAtl !== fees24hAtl.fees24h ||
+                    dbPool.dynamicData.fees24hAtlTimestamp !== fees24hAtl.timestamp
+                ) {
+                    updates.push(
+                        prisma.prismaPoolDynamicData.update({
+                            where: { id_chain: { id: pool.id, chain: this.chain } },
+                            data: {
+                                sharePriceAth: sharePriceAth.sharePrice,
+                                sharePriceAthTimestamp: sharePriceAth.timestamp,
+                                sharePriceAtl: sharePriceAtl.sharePrice,
+                                sharePriceAtlTimestamp: sharePriceAtl.timestamp,
+
+                                totalLiquidityAth: totalLiquidityAth.totalLiquidity,
+                                totalLiquidityAthTimestamp: totalLiquidityAth.timestamp,
+                                totalLiquidityAtl: totalLiquidityAtl.totalLiquidity,
+                                totalLiquidityAtlTimestamp: totalLiquidityAtl.timestamp,
+
+                                volume24hAth: volume24hAth.volume24h,
+                                volume24hAthTimestamp: volume24hAth.timestamp,
+                                volume24hAtl: volume24hAtl.volume24h,
+                                volume24hAtlTimestamp: volume24hAtl.timestamp,
+
+                                fees24hAth: fees24hAth.fees24h,
+                                fees24hAthTimestamp: fees24hAth.timestamp,
+                                fees24hAtl: fees24hAtl.fees24h,
+                                fees24hAtlTimestamp: fees24hAtl.timestamp,
+                            },
+                        }),
+                    );
+                }
             }
         }
 
