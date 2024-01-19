14ca39fb59e3f5d82eb074921d1687b6ebf46c93
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-01-19 12:56:09 +0100

    adding indexes on user addresses in balance tables

---
+++ prisma/migrations/20240119115442_user_address_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "PrismaUserStakedBalance_userAddress_idx" ON "PrismaUserStakedBalance"("userAddress");
+
+-- CreateIndex
+CREATE INDEX "PrismaUserWalletBalance_userAddress_idx" ON "PrismaUserWalletBalance"("userAddress");
