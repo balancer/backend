44e470e602cb20694cf240f996a3fa36d4c37ec0
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-01-23 13:00:16 +0100

    PoolDynamicData indexes

---
+++ prisma/migrations/20240123115918_dynamic_data_indexes/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "PrismaPoolDynamicData_totalLiquidity_idx" ON "PrismaPoolDynamicData"("totalLiquidity");
+
+-- CreateIndex
+CREATE INDEX "PrismaPoolDynamicData_totalShares_idx" ON "PrismaPoolDynamicData"("totalShares");
+
+-- CreateIndex
+CREATE INDEX "PrismaPoolDynamicData_volume24h_idx" ON "PrismaPoolDynamicData"("volume24h");
+
+-- CreateIndex
+CREATE INDEX "PrismaPoolDynamicData_apr_idx" ON "PrismaPoolDynamicData"("apr");
