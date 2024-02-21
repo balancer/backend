5130fe533a8b21d90c6a0dc782a7476bf2f377b2
franz <franzns11@gmail.com>
2024-02-21 17:25:45 +0100

    wip

--- modules/sources/contracts/fetch-pool-tokens.ts
+++ modules/sources/contracts/fetch-pool-tokens.ts
@@ -1,5 +1,6 @@
 import { parseAbi } from 'viem';
 import { ViemClient } from '../types';
+import { vaultV3Abi } from './abis/VaultV3';
 
 type PoolTokenInfo = {
     tokens: `0x${string}`[];
@@ -18,14 +19,21 @@ export async function fetchPoolTokens(vault: string, pools: string[], client: Vi
         .map((pool) => [
             {
                 address: vault as `0x${string}`,
-                abi,
-                args: [pool as `0x${string}`],
+                abi: vaultV3Abi,
                 functionName: 'getPoolTokenInfo',
+                args: [pool as `0x${string}`],
             },
         ])
         .flat();
 
     const results = await client.multicall({ contracts });
+
+    const data = await client.readContract({
+        address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
+        abi: vaultV3Abi,
+        functionName: 'getPoolTokenInfo' as 'getPoolTokenInfo',
+        args: ['0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC'],
+    });
 
     // Parse the results
     const parsedResults = results
