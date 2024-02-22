49ea3ee316c3bdc5b9e983993216c8354cdb6220
Alberto Gualis <agualis@gmail.com>
2024-02-22 09:53:24 +0100

    Add function to create viem read contract args

--- modules/sources/contracts/fetch-pool-tokens.ts
+++ modules/sources/contracts/fetch-pool-tokens.ts
@@ -1,4 +1,4 @@
-import { parseAbi } from 'viem';
+import { Abi, ContractFunctionArgs, ContractFunctionName, ReadContractParameters, parseAbi } from 'viem';
 import { ViemClient } from '../types';
 import { vaultV3Abi } from './abis/VaultV3';
 
@@ -14,19 +14,39 @@ const abi = parseAbi([
     'function getPoolTokenInfo(address pool) view returns (address[] tokens, uint8[] tokenTypes, uint[] balancesRaw, uint[] decimalScalingFactors, address[] rateProviders)',
 ]);
 
-export async function fetchPoolTokens(vault: string, pools: string[], client: ViemClient) {
-    const contracts = pools
-        .map((pool) => [
-            {
-                address: vault as `0x${string}`,
-                abi: vaultV3Abi,
-                functionName: 'getPoolTokenInfo',
-                args: [pool as `0x${string}`],
-            },
-        ])
-        .flat();
 
-    const results = await client.multicall({ contracts });
+export async function fetchPoolTokens(vault: string, pools: string[], client: ViemClient) {
+
+
+    function createArgs<
+        ABI extends Abi | readonly unknown[],
+        FunctionName extends ContractFunctionName<ABI, 'pure' | 'view'>,
+        Args extends ContractFunctionArgs<ABI, 'pure' | 'view', FunctionName>,
+    >({abi, functionName, args}: {abi: ABI, functionName: FunctionName, args: Args})
+    : ReadContractParameters<ABI, FunctionName, Args> {
+    return {
+            abi, functionName, args
+        } as ReadContractParameters<ABI, FunctionName, Args>;
+    }
+
+  const contracts = pools
+  .map((pool) => [
+      {
+          ...createArgs({
+            abi: vaultV3Abi,
+            functionName: 'getPoolTokenInfo',
+            args: [pool as `0x${string}`]}),
+          address: vault as `0x${string}`,
+      } as const,
+      {
+          ...createArgs({abi: vaultV3Abi, functionName: 'getPoolTokenRates', args: [pool as `0x${string}`]}),
+          address: vault as `0x${string}`,
+      } as const,
+  ])
+  .flat();
+
+
+    const results = await client.multicall({ contracts: contracts });
 
     const data = await client.readContract({
         address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
