0b9a9b2136b63747f1f79083a7bc070d8fc84952
Xeonus <vanderbourg@gmail.com>
2024-02-27 14:57:49 +0100

    Hotfix: exclude sDOLA
    - Exclude sDOLA so that pricing information is properly handled as there is a coingecko id but not price
    - Affected pool: https://app.balancer.fi/#/ethereum/pool/0x264062ca46a1322c2e6464471764089e01f22f1900000000000000000000066b

--- modules/network/mainnet.ts
+++ modules/network/mainnet.ts
@@ -58,7 +58,8 @@ export const data: NetworkData = {
         nativeAssetId: 'ethereum',
         platformId: 'ethereum',
         excludedTokenAddresses: [
-            '0x04c154b66cb340f3ae24111cc767e0184ed00cc6', // pxETH, has coingekco entry but no price
+            '0x04c154b66cb340f3ae24111cc767e0184ed00cc6', // pxETH, has Coingecko entry but no price
+            '0x865377367054516e17014CcdED1e7d814EDC9ce4', // sDOLA, has Coingecko entry but no price
         ],
     },
     rpcUrl:
