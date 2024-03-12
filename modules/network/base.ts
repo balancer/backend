b28c997c592e715cbcf8f92c048f305c87e0cc03
franzns <93920061+franzns@users.noreply.github.com>
2024-03-12 13:13:00 +0200

    fix the base queries contract address (#207)

--- modules/network/base.ts
+++ modules/network/base.ts
@@ -61,7 +61,7 @@ const baseNetworkData: NetworkData = {
             vaultAddress: '0xba12222222228d8ba445958a75a0704d566bf2c8',
             defaultSwapFeePercentage: '0.5',
             defaultYieldFeePercentage: '0.5',
-            balancerQueriesAddress: '0xe39b5e3b6d74016b2f6a9673d7d7493b6df549d5',
+            balancerQueriesAddress: '0x300ab2038eac391f26d9f895dc61f8f66a548833',
         },
         v3: {
             vaultAddress: '0xba12222222228d8ba445958a75a0704d566bf2c8',
