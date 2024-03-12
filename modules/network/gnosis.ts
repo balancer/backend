32f69bf3d1c1be52b91462b914cc71c560178bb9
franz <franzns11@gmail.com>
2024-03-12 13:53:48 +0200

    dont run on dev, give more time

--- modules/network/gnosis.ts
+++ modules/network/gnosis.ts
@@ -220,7 +220,7 @@ export const gnosisNetworkConfig: NetworkConfig = {
         },
         {
             name: 'feed-data-to-datastudio',
-            interval: (env.DEPLOYMENT_ENV as DeploymentEnv) === 'canary' ? every(5, 'minutes') : every(5, 'minutes'),
+            interval: (env.DEPLOYMENT_ENV as DeploymentEnv) === 'canary' ? every(10, 'minutes') : every(10, 'minutes'),
         },
     ],
 };
