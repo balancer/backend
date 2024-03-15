170a91db1b2d20c1186f8b908c53ee1d6c7c6127
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-03-15 16:55:42 +0100

    token prices index and join/exit sync on v2

--- modules/controllers/jobs-controller.ts
+++ modules/controllers/jobs-controller.ts
@@ -32,8 +32,6 @@ export function JobsController(tracer?: any) {
     // ...
     return {
         async syncJoinExitsV2(chainId: string) {
-            return true; // TODO: disable syncing because of high DB usage, probably due to USD pricing fetching
-
             const chain = chainIdToChain[chainId];
             const {
                 subgraphs: { balancer },
