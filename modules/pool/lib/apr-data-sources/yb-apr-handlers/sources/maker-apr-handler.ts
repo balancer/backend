bf021003798194542fa640d62dd003bc77f612b2
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-04-23 22:07:11 +0200

    maker sdai APRs

--- modules/pool/lib/apr-data-sources/yb-apr-handlers/sources/maker-apr-handler.ts
+++ modules/pool/lib/apr-data-sources/yb-apr-handlers/sources/maker-apr-handler.ts
@@ -1,40 +1,49 @@
 import { AprHandler } from '..';
 import { MakerAprConfig } from '../../../../../network/apr-config-types';
-import { getContractAt } from '../../../../../web3/contract';
 import { abi as makerPotAbi } from './abis/maker-pot';
+import config from '../../../../../../config';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+import { Chain } from '@prisma/client';
+
+const client = createPublicClient({
+    chain: mainnet,
+    transport: http(config.MAINNET.rpcUrl),
+});
+
+const potAddress = '0x197e90f9fad81970ba7976f33cbd77088e5d7cf7';
+const sdai = {
+    [Chain.MAINNET]: '0x83f20f44975d03b1b09e64809b757c47f942beea',
+    [Chain.BASE]: '0x99ac4484e8a1dbd6a185380b3a811913ac884d87',
+    [Chain.OPTIMISM]: '0x2218a117083f5b482b0bb821d27056ba9c04b1d3',
+};
 
 export class MakerAprHandler implements AprHandler {
     group = 'MAKER';
-    tokens: {
-        [tokenName: string]: {
-            address: string;
-            potAddress: string;
-            isIbYield?: boolean;
-        };
-    };
+    private sdai: string;
 
-    constructor(aprConfig: MakerAprConfig) {
-        this.tokens = aprConfig.tokens;
+    constructor({ sdai }: MakerAprConfig) {
+        this.sdai = sdai;
     }
 
     async getAprs() {
         const aprs: { [p: string]: { apr: number; isIbYield: boolean; group: string } } = {};
-        for (const { address, potAddress, isIbYield } of Object.values(this.tokens)) {
-            try {
-                const potContract = getContractAt(potAddress, makerPotAbi);
-                const dsr = await potContract.dsr();
-                if (!dsr) {
-                    continue;
-                }
-                const tokenApr = (Number(dsr) * 10 ** -27 - 1) * 365 * 24 * 60 * 60;
-                aprs[address] = {
-                    apr: tokenApr,
-                    isIbYield: isIbYield ?? false,
-                    group: this.group,
-                };
-            } catch (error) {
-                console.error(`Maker APR Failed for token ${address}: `, error);
-            }
+        try {
+            const dsr = await client.readContract({
+                abi: makerPotAbi,
+                address: potAddress,
+                functionName: 'dsr',
+            });
+
+            const tokenApr = (Number(dsr) * 10 ** -27 - 1) * 365 * 24 * 60 * 60;
+
+            aprs[this.sdai] = {
+                apr: tokenApr,
+                isIbYield: false,
+                group: this.group,
+            };
+        } catch (error) {
+            console.error(`Maker APR Failed for token ${this.sdai}: `, error);
         }
         return aprs;
     }
