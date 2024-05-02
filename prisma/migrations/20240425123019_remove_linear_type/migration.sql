06dc41b3e895967dfd74ef12375ea7b1430b2ee6
franz <franzns11@gmail.com>
2024-05-02 11:34:08 +0200

    delete linear pools

--- prisma/migrations/20240425123019_remove_linear_type/migration.sql
+++ prisma/migrations/20240425123019_remove_linear_type/migration.sql
@@ -2,10 +2,12 @@
   Warnings:
 
   - The values [LINEAR] on the enum `PrismaPoolType` will be removed. If these variants are still used in the database, this will fail.
+  - Will delete all pools with type=LINEAR first
 
 */
 -- AlterEnum
 BEGIN;
+DELETE FROM "PrismaPool" WHERE type='LINEAR';
 CREATE TYPE "PrismaPoolType_new" AS ENUM ('WEIGHTED', 'STABLE', 'META_STABLE', 'PHANTOM_STABLE', 'COMPOSABLE_STABLE', 'ELEMENT', 'UNKNOWN', 'LIQUIDITY_BOOTSTRAPPING', 'INVESTMENT', 'GYRO', 'GYRO3', 'GYROE', 'FX');
 ALTER TABLE "PrismaPool" ALTER COLUMN "type" TYPE "PrismaPoolType_new" USING ("type"::text::"PrismaPoolType_new");
 ALTER TYPE "PrismaPoolType" RENAME TO "PrismaPoolType_old";
