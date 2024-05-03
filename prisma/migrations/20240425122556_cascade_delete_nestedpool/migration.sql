46ac39eba260547d3939714596af761643498f26
franz <franzns11@gmail.com>
2024-05-03 15:15:16 +0200

    adjust schema and db

--- prisma/migrations/20240425122556_cascade_delete_nestedpool/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "PrismaPoolToken" DROP CONSTRAINT "PrismaPoolToken_nestedPoolId_chain_fkey";
-
--- AddForeignKey
-ALTER TABLE "PrismaPoolToken" ADD CONSTRAINT "PrismaPoolToken_nestedPoolId_chain_fkey" FOREIGN KEY ("nestedPoolId", "chain") REFERENCES "PrismaPool"("id", "chain") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20240425122721_delete_cascade_poolcategory/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "PrismaPoolCategory" DROP CONSTRAINT "PrismaPoolCategory_poolId_chain_fkey";
-
--- AddForeignKey
-ALTER TABLE "PrismaPoolCategory" ADD CONSTRAINT "PrismaPoolCategory_poolId_chain_fkey" FOREIGN KEY ("poolId", "chain") REFERENCES "PrismaPool"("id", "chain") ON DELETE CASCADE ON UPDATE CASCADE;
