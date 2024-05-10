418f3d497a822b9bdc2788aa0b543e2f25f5b22c
franzns <93920061+franzns@users.noreply.github.com>
2024-05-10 12:06:36 +0200

    prioritize alchemy over infura on arb (#392)

--- config/arbitrum.ts
+++ config/arbitrum.ts
@@ -34,12 +34,11 @@ export default <NetworkData>{
         platformId: 'arbitrum-one',
         excludedTokenAddresses: ['0x6dbf2155b0636cb3fd5359fccefb8a2c02b6cb51'], // plsRDNT, has coingecko entry but no price
     },
-    rpcUrl:
-        env.INFURA_API_KEY && (env.DEPLOYMENT_ENV as DeploymentEnv) === 'main'
-            ? `https://arbitrum-mainnet.infura.io/v3/${env.INFURA_API_KEY}`
-            : env.ALCHEMY_API_KEY
-            ? `https://arb-mainnet.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`
-            : 'https://1rpc.io/arb',
+    rpcUrl: env.ALCHEMY_API_KEY
+        ? `https://arb-mainnet.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`
+        : env.INFURA_API_KEY
+        ? `https://arbitrum-mainnet.infura.io/v3/${env.INFURA_API_KEY}`
+        : 'https://1rpc.io/arb',
     rpcMaxBlockRange: 2000,
     protocolToken: 'bal',
     bal: {
