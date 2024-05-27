32f0c5efa9082d08255bb77e55da0d38576153c8
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-05-27 15:27:49 +0200

    New chain configs: Fraxtal and Mode (#426)
    * viem update
    
    * new: fraxtal config
    
    * new: mode config

--- modules/network/network-config.ts
+++ modules/network/network-config.ts
@@ -8,8 +8,10 @@ import { gnosisNetworkConfig } from './gnosis';
 import { zkevmNetworkConfig } from './zkevm';
 import { avalancheNetworkConfig } from './avalanche';
 import { baseNetworkConfig } from './base';
+import { fraxtalNetworkConfig } from './fraxtal';
 import { Chain } from '@prisma/client';
 import { sepoliaNetworkConfig } from './sepolia';
+import { modeNetworkConfig } from './mode';
 
 export const AllNetworkConfigs: { [chainId: string]: NetworkConfig } = {
     '250': fantomNetworkConfig,
@@ -22,6 +24,8 @@ export const AllNetworkConfigs: { [chainId: string]: NetworkConfig } = {
     '43114': avalancheNetworkConfig,
     '8453': baseNetworkConfig,
     '11155111': sepoliaNetworkConfig,
+    '252': fraxtalNetworkConfig,
+    '34443': modeNetworkConfig,
 };
 
 export const AllNetworkConfigsKeyedOnChain: { [chain in Chain]: NetworkConfig } = {
@@ -35,9 +39,11 @@ export const AllNetworkConfigsKeyedOnChain: { [chain in Chain]: NetworkConfig } 
     AVALANCHE: avalancheNetworkConfig,
     BASE: baseNetworkConfig,
     SEPOLIA: sepoliaNetworkConfig,
+    FRAXTAL: fraxtalNetworkConfig,
+    MODE: modeNetworkConfig,
 };
 
-export const BalancerChainIds = ['1', '137', '42161', '100', '1101', '43114', '8453', '11155111'];
+export const BalancerChainIds = ['1', '137', '42161', '100', '1101', '43114', '8453', '11155111', '252', '34443'];
 export const BeethovenChainIds = ['250', '10'];
 
 export const chainToIdMap = Object.values(AllNetworkConfigs).reduce((acc, config) => {
