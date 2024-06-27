885607dd7d1209dde54c2aefa4cac9f3a3677783
franz <franzns11@gmail.com>
2024-06-27 14:30:30 +0200

    expose surplus

--- modules/pool/pool.resolvers.ts
+++ modules/pool/pool.resolvers.ts
@@ -93,6 +93,7 @@ const balancerResolvers: Resolvers = {
                 sharePrice: `${snapshot.sharePrice}`,
                 volume24h: `${snapshot.volume24h}`,
                 fees24h: `${snapshot.fees24h}`,
+                surplus24h: `${snapshot.surplus24h}`,
                 totalSwapVolume: `${snapshot.totalSwapVolume}`,
                 totalSwapFee: `${snapshot.totalSwapFee}`,
                 totalSurplus: `${snapshot.totalSurplus}`,
