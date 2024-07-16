2dbbb7c44381e153abc7ee0e68b5b1550fe0bfa7
franzns <93920061+franzns@users.noreply.github.com>
2024-07-16 17:51:01 +0200

    Hf/cow sync (#622)
    * fix sync when no pools are there
    
    * changeset

---
+++ .changeset/shy-cobras-develop.md
@@ -0,0 +1,5 @@
+---
+'backend': patch
+---
+
+fix initial cow amm sync
--- modules/actions/cow-amm/fetch-changed-pools.ts
+++ modules/actions/cow-amm/fetch-changed-pools.ts
@@ -8,7 +8,7 @@ export const fetchChangedPools = async (viemClient: ViemClient, chain: Chain, fr
         .findMany({
             where: {
                 chain,
-                protocolVersion: 0,
+                protocolVersion: 1,
             },
             select: {
                 id: true,
