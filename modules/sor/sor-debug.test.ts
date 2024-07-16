92695242d9b0cd9d1370786d242f6af0dc61521b
franz <franzns11@gmail.com>
2024-07-16 08:30:26 +0200

    fix test

--- modules/sor/sor-debug.test.ts
+++ modules/sor/sor-debug.test.ts
@@ -4,6 +4,15 @@ import { initRequestScopedContext, setRequestScopedContextValue } from '../conte
 import { poolService } from '../pool/pool.service';
 import { sorService } from './sor.service';
 import { chainIdToChain } from '../network/chain-id-to-chain';
+import {
+    JobsController,
+    SnapshotsController,
+    PoolsMutationController,
+    UserBalancesController,
+    CowAmmController,
+    AprsController,
+    ContentController,
+} from '../controllers';
 
 describe('sor debugging', () => {
     it('sor v2 arb eth->usdc', async () => {
@@ -16,6 +25,36 @@ describe('sor debugging', () => {
         await poolService.syncAllPoolsFromSubgraph();
         await poolService.loadOnChainDataForAllPools();
         await poolService.updateLiquidityValuesForPools();
+
+        const swaps = await sorService.getSorSwapPaths({
+            chain,
+            tokenIn: '0xb19382073c7A0aDdbb56Ac6AF1808Fa49e377B75', // BAL
+            tokenOut: '0x7b79995e5f793a07bc00c21412e50ecae098e7f9', // WETH
+            swapType: 'EXACT_IN',
+            swapAmount: '1',
+            queryBatchSwap: false,
+            useProtocolVersion: 3,
+            // callDataInput: {
+            //     receiver: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
+            //     sender: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
+            //     slippagePercentage: '0.1',
+            // },
+        });
+
+        console.log(swaps.returnAmount);
+        expect(parseFloat(swaps.returnAmount)).toBeGreaterThan(0);
+    }, 5000000);
+
+    it('sor v3 sepolia eth->usdc', async () => {
+        const jobsController = JobsController();
+        const chain = Chain.SEPOLIA;
+
+        const chainId = Object.keys(chainIdToChain).find((key) => chainIdToChain[key] === chain) as string;
+        initRequestScopedContext();
+        setRequestScopedContextValue('chainId', chainId);
+        //only do once before starting to debug
+        await jobsController.addPools(chain);
+        await jobsController.syncPools(chain);
 
         const swaps = await sorService.getSorSwapPaths({
             chain,
