3fbc8220730b84b10f607cc5577a40eb3644b345
franz <franzns11@gmail.com>
2024-07-25 21:27:23 +0200

    update subgraphs, add gnosis

--- config/mainnet.ts
+++ config/mainnet.ts
@@ -20,7 +20,7 @@ export default <NetworkData>{
     },
     subgraphs: {
         startDate: '2019-04-20',
-        cowAmm: `https://gateway-arbitrum.network.thegraph.com/api/${env.THEGRAPH_API_KEY_BALANCER}/deployments/id/QmZiLiHYhohsLXdVy8cz3uvqDRpBpyaG5G6SYeJHBt2WLv`,
+        cowAmm: `https://gateway-arbitrum.network.thegraph.com/api/${env.THEGRAPH_API_KEY_BALANCER}/deployments/id/QmU7UjxpKihYrL1Pvx4JPZcmecnd6rCuXuvMzLJK5yC2FE`,
         balancer: `https://gateway-arbitrum.network.thegraph.com/api/${env.THEGRAPH_API_KEY_BALANCER}/deployments/id/QmQ5TT2yYBZgoUxsat3bKmNe5Fr9LW9YAtDs8aeuc1BRhj`,
         beetsBar: 'https://',
         blocks: 'https://api.studio.thegraph.com/query/48427/ethereum-blocks/version/latest',
