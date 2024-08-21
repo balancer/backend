98a55f85b4a6fd5dea479880724634e39feedd20
franz <franzns11@gmail.com>
2024-08-21 16:43:31 +0200

    handle streamed bal on mainnet properly

--- modules/pool/lib/apr-data-sources/ve-bal-gauge-apr.service.ts
+++ modules/pool/lib/apr-data-sources/ve-bal-gauge-apr.service.ts
@@ -58,7 +58,7 @@ export class GaugeAprService implements PoolAprService {
 
             // Get token rewards per year with data needed for the DB
             const rewards = await Promise.allSettled(
-                gauge.rewards.map(async ({ tokenAddress, rewardPerSecond }) => {
+                gauge.rewards.map(async ({ id, tokenAddress, rewardPerSecond }) => {
                     const price = this.tokenService.getPriceForToken(tokenPrices, tokenAddress, networkContext.chain);
                     if (!price) {
                         return Promise.reject(`Price not found for ${tokenAddress}`);
@@ -75,6 +75,7 @@ export class GaugeAprService implements PoolAprService {
                     }
 
                     return {
+                        id: id,
                         address: tokenAddress,
                         symbol: definition.symbol,
                         rewardPerYear: parseFloat(rewardPerSecond) * secondsPerYear * price,
@@ -101,7 +102,7 @@ export class GaugeAprService implements PoolAprService {
                     const { address, symbol, rewardPerYear } = reward.value;
 
                     const itemData: PrismaPoolAprItem = {
-                        id: `${gauge.id}-${symbol}-apr`,
+                        id: `${reward.value.id}-${symbol}-apr`,
                         chain: networkContext.chain,
                         poolId: pool.id,
                         title: `${symbol} reward APR`,
