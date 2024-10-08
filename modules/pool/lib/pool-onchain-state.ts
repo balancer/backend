55e61029ced80bac4584f1416b876e2c5d6ad928
gmbronco <83549293+gmbronco@users.noreply.github.com>
2024-10-08 07:14:15 +0200

    fix: paused state parsing issue

--- modules/pool/lib/pool-onchain-state.ts
+++ modules/pool/lib/pool-onchain-state.ts
@@ -2,7 +2,6 @@ import { Multicaller3Viem } from '../../web3/multicaller-viem';
 import { PrismaPoolType } from '@prisma/client';
 import abi from '../abi/WeightedPoolV2.json';
 import { Chain } from '@prisma/client';
-import { Multicaller3 } from '../../web3/multicaller3';
 
 interface PoolInput {
     id: string;
@@ -13,14 +12,12 @@ interface PoolInput {
 }
 
 interface OnchainState {
-    pausedState?: {
-        paused: boolean;
-    };
+    pausedState?: [boolean, bigint, bigint];
     inRecoveryMode?: boolean;
 }
 
 const parse = (result: OnchainState) => ({
-    isPaused: result.pausedState?.paused ? result.pausedState.paused : false,
+    isPaused: result.pausedState ? result.pausedState[0] : false,
     isInRecoveryMode: result.inRecoveryMode ? result.inRecoveryMode : false,
 });
 
@@ -29,7 +26,7 @@ export const fetchOnChainPoolState = async (pools: PoolInput[], batchSize = 1024
         return {};
     }
 
-    const multicaller = new Multicaller3(abi, batchSize);
+    const multicaller = new Multicaller3Viem(pools[0].chain, abi, batchSize);
 
     pools.forEach(({ id, type, address }) => {
         // filter certain pool types that don't have pausedState or recovery mode
