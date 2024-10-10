ebc0eaf6193e52f6ca11a1a0f3b5736d2faaf36a
mkflow27 <mkflow27@gmail.com>
2024-10-10 16:04:31 +0200

    feat: allow stable pool routing for v3 (#1044)
    * feat: allow stable pool routing for v3
    
    * Create wise-dodos-sell.md

---
+++ .changeset/wise-dodos-sell.md
@@ -0,0 +1,5 @@
+---
+"backend": patch
+---
+
+feat: allow stable pool routing for v3
--- modules/sor/sorV2/lib/static.ts
+++ modules/sor/sorV2/lib/static.ts
@@ -43,7 +43,7 @@ export async function sorGetPathsWithPools(
                     [
                         '0x3dd0843a028c86e0b760b1a76929d1c5ef93a2dd000200000000000000000249', // auraBal/8020
                         '0x2d011adf89f0576c9b722c28269fcb5d50c2d17900020000000000000000024d', // sdBal/8020
-                    ].includes(prismaPool.id)
+                    ].includes(prismaPool.id) || protocolVersion === 3
                 ) {
                     basePools.push(StablePool.fromPrismaPool(prismaPool));
                 }
