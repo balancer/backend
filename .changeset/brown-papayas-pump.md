7163e598abdb13f831b2ab681981c1be2830ecf6
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-02-04 18:34:31 +0100

    fix missing underlying error handling

---
+++ .changeset/brown-papayas-pump.md
@@ -0,0 +1,5 @@
+---
+'backend': patch
+---
+
+fix missing underlying error handling
--- modules/sources/contracts/v3/fetch-unwrap-rates.ts
+++ modules/sources/contracts/v3/fetch-unwrap-rates.ts
@@ -17,22 +17,36 @@ export const fetchUnwrapRates = async (
     if (erc4626Tokens.length === 0) {
         return {};
     }
-    const chain = erc4626Tokens[0].chain;
+    // Guard against tokens with missing underlying token
+    const validTokens: PrismaToken[] = [];
+    const missingTokens: PrismaToken[] = [];
+
+    for (const token of erc4626Tokens) {
+        const hasUnderlying = token.underlyingTokenAddress && underlyingTokenMap[token.underlyingTokenAddress];
+
+        if (hasUnderlying) {
+            validTokens.push(token);
+        } else {
+            missingTokens.push(token);
+        }
+    }
+
+    if (missingTokens.length) {
+        console.error(
+            'Missing underlying token in ERC4626 tokens',
+            missingTokens.map(({ address, chain }) => [address, chain]),
+        );
+    }
+    const chain = validTokens[0].chain;
     const caller = new Multicaller3Viem(chain, MinimalErc4626Abi);
-    erc4626Tokens.forEach((token) => caller.call(token.address, token.address, 'convertToAssets', [parseEther('1')]));
+    validTokens.forEach((token) => caller.call(token.address, token.address, 'convertToAssets', [parseEther('1')]));
     const results = await caller.execute<{ [id: string]: bigint }>();
 
     // Convert the results to floats
     const formattedResults = Object.fromEntries(
         Object.entries(results).map(([key, value], index) => {
-            const token = erc4626Tokens[index];
-            const underlyingToken = token.underlyingTokenAddress
-                ? underlyingTokenMap[token.underlyingTokenAddress]
-                : null;
-            if (!underlyingToken) {
-                console.error(`Missing underlying token for ${key}`);
-                return [key, { unwrapRate: '1', unwrapRateDecimals: 18 }];
-            }
+            const token = validTokens[index];
+            const underlyingToken = underlyingTokenMap[token.underlyingTokenAddress!];
             const unwrapRateDecimals = 18 - token.decimals + underlyingToken.decimals;
             return [key, formatUnits(value, unwrapRateDecimals)];
         }),
