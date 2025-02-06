3e0c530c4c2cb5d152f6cf6b99045e2a94e32559
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-02-06 16:38:36 +0100

    fixes

--- modules/pool/lib/apr-data-sources/morpho-rewards-apr.service.ts
+++ modules/pool/lib/apr-data-sources/morpho-rewards-apr.service.ts
@@ -1,9 +1,10 @@
 import { PoolAprService } from '../../pool-types';
 import { PrismaPoolWithTokens } from '../../../../prisma/prisma-types';
 import { prisma } from '../../../../prisma/prisma-client';
-import { PrismaPoolAprItemGroup, PrismaPoolAprType } from '@prisma/client';
+import { PrismaPoolAprType } from '@prisma/client';
 import { morphoApiClient } from './morpho-api-client';
-import murmurhash from 'murmurhash';
+// IDs can be converted to hashes for DB perf optimization
+// import murmurhash from 'murmurhash';
 
 const MORPHO_TOKEN = '0x58d97b57bb95320f9a05dc918aef65434969c2b2';
 
@@ -34,14 +35,14 @@ export class MorphoRewardsAprService implements PoolAprService {
         const morphoApr = await morphoApiClient.morphoApr();
 
         // Find all pools with Morpho vault tokens
-        const morphoAddresses = Object.keys(morphoRewards);
+        const morphoVaultAddresses = Object.keys(morphoRewards);
         const poolsWithMorphoTokens = pools.filter((pool) => {
-            return pool.tokens.find((token) => morphoAddresses.includes(token.address));
+            return pool.tokens.find((token) => morphoVaultAddresses.includes(token.address));
         });
 
         // For each of them get reward token APRs
         const aprItems = poolsWithMorphoTokens.flatMap((pool) => {
-            const tokens = pool.tokens.filter((token) => morphoAddresses.includes(token.address));
+            const tokens = pool.tokens.filter((token) => morphoVaultAddresses.includes(token.address));
             const tvl = pool.tokens.map((t) => t.balanceUSD).reduce((a, b) => a + b, 0);
             const morphoShare = tokens.map((t) => (tvl > 0 ? t.balanceUSD / tvl : 0)).reduce((a, b) => a + b, 0);
 
@@ -50,12 +51,12 @@ export class MorphoRewardsAprService implements PoolAprService {
                 const weight = token.balanceUSD / tvl;
 
                 return rewardTokens.map((rewardToken) => ({
-                    id: murmurhash.v3(`${pool.id}-${token.address}-${rewardToken.address}`).toString(36),
+                    // id: murmurhash.v3(`${pool.id}-${token.address}-${rewardToken.address}`).toString(36),
+                    id: `${pool.id}-${token.address}-${rewardToken.address}`,
                     chain: pool.chain,
                     poolId: pool.id,
                     title: `${rewardToken.symbol} APR`,
                     apr: rewardToken.apr * weight,
-                    group: PrismaPoolAprItemGroup.MORPHO,
                     type: PrismaPoolAprType.MERKL,
                     rewardTokenAddress: rewardToken.address,
                     rewardTokenSymbol: rewardToken.symbol,
@@ -64,12 +65,12 @@ export class MorphoRewardsAprService implements PoolAprService {
 
             // Each pool with Morpho vault is receiving a MORPHO token on top of the rewards
             const morphoAprItem = {
-                id: murmurhash.v3(`${pool.id}-morpho`).toString(36),
+                // id: murmurhash.v3(`${pool.id}-morpho`).toString(36),
+                id: `${pool.id}-morpho`,
                 chain: pool.chain,
                 poolId: pool.id,
                 title: 'MORPHO APR',
                 apr: morphoApr[pool.chain as keyof typeof morphoApr] * morphoShare,
-                group: PrismaPoolAprItemGroup.MORPHO,
                 type: PrismaPoolAprType.MERKL,
                 rewardTokenAddress: MORPHO_TOKEN,
                 rewardTokenSymbol: 'MORPHO',
