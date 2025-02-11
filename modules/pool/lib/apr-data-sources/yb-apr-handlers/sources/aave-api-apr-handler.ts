17b9d914d0f44c8d40d1c55d88feeff24299b780
franz <franzns11@gmail.com>
2025-02-11 12:59:01 +0100

    wip

---
+++ modules/pool/lib/apr-data-sources/yb-apr-handlers/sources/aave-api-apr-handler.ts
@@ -0,0 +1,46 @@
+import { BeefyAprConfig } from '../../../../../network/apr-config-types';
+import { AprHandler } from '../types';
+import axios from 'axios';
+
+export class AaveApiHandler implements AprHandler {
+    tokens: {
+        [tokenName: string]: {
+            address: string;
+            vaultId: string;
+            isIbYield?: boolean;
+        };
+    };
+    sourceUrl: string;
+    group = 'AAVE';
+
+    constructor(config: BeefyAprConfig) {
+        this.tokens = config.tokens;
+        this.sourceUrl = config.sourceUrl;
+    }
+
+    async getAprs() {
+        try {
+            const { data: aprData } = await axios.get<VaultApr>(this.sourceUrl);
+            const aprs = Object.values(this.tokens)
+                .map(({ address, vaultId, isIbYield }) => {
+                    const apr = aprData[vaultId] ?? 0;
+                    return {
+                        [address]: {
+                            apr,
+                            isIbYield: isIbYield ?? false,
+                            group: this.group,
+                        },
+                    };
+                })
+                .flat()
+                .reduce((acc, curr) => ({ ...acc, ...curr }), {});
+
+            return aprs;
+        } catch (error) {
+            console.error(`Beefy IB APR hanlder failed: `, error);
+            return {};
+        }
+    }
+}
+
+type VaultApr = Record<string, number>;
