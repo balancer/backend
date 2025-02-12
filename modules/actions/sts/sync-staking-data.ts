825da2fa8364ac6c2483d4c18c78bbda463f02b6
franz <franzns11@gmail.com>
2025-02-12 16:43:19 +0100

    wip

--- modules/actions/sts/sync-staking-data.ts
+++ modules/actions/sts/sync-staking-data.ts
@@ -3,8 +3,9 @@ import { fetchSonicStakingData } from '../../sources/contracts/fetch-sts-staking
 import { StsSubgraphService } from '../../sources/subgraphs/sts-subgraph/sts.service';
 import { Address, formatEther } from 'viem';
 import { ViemClient } from '../../sources/viem-client';
+import { blockNumbers } from '../../block-numbers';
 
-interface ApiResponse {
+interface SonicApiResponse {
     success: boolean;
     data: {
         apr: number;
@@ -20,9 +21,18 @@ export async function syncStakingData(
 ) {
     const stakingDataOnchain = await fetchSonicStakingData(stakingContractAddress, viemClient);
     const validators = await subgraphService.getAllValidators();
+    const latestStakingData = await subgraphService.getStakingData();
+    const block24HrsAgo = await blockNumbers().getBlock('SONIC', Date.now() - 24 * 60 * 60 * 1000);
+    const stakingData24hrsAgo = await subgraphService.getStakingData(block24HrsAgo);
+
+    let protocolRevenue24hrs = 0;
+    if (latestStakingData && stakingData24hrsAgo) {
+        protocolRevenue24hrs =
+            parseFloat(latestStakingData.totalProtocolFee) - parseFloat(stakingData24hrsAgo.totalProtocolFee);
+    }
 
     const response = await fetch(baseAprUrl);
-    const data = (await response.json()) as ApiResponse;
+    const data = (await response.json()) as SonicApiResponse;
     if (!data.success) {
         throw new Error('Failed to fetch sonic staking APR');
     }
