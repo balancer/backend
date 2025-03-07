9d9a76d5d03b55ab7c0e45cde82db499b7039eb7
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-03-07 16:59:50 +0100

    use viem for vebal sync service

--- modules/user/lib/user-sync-vebal-lock-balance.service.ts
+++ modules/user/lib/user-sync-vebal-lock-balance.service.ts
@@ -1,28 +1,21 @@
 import { UserStakedBalanceService, UserSyncUserBalanceInput } from '../user-types';
 import { prisma } from '../../../prisma/prisma-client';
-import { getContractAt } from '../../web3/contract';
 import _ from 'lodash';
 import { prismaBulkExecuteOperations } from '../../../prisma/prisma-util';
-import RewardsOnlyGaugeAbi from './abi/RewardsOnlyGauge.json';
-import { Multicaller } from '../../web3/multicaller';
 import { formatFixed } from '@ethersproject/bignumber';
-import { PrismaPoolStakingType } from '@prisma/client';
-import { networkContext } from '../../network/network-context.service';
-import { GaugeSubgraphService } from '../../subgraphs/gauge-subgraph/gauge-subgraph.service';
+import { Chain, PrismaPoolStakingType } from '@prisma/client';
 import { veBalLocksSubgraphService } from '../../subgraphs/veBal-locks-subgraph/veBal-locks-subgraph.service';
 import { BigNumber } from 'ethers';
 import VeBalABI from '../../vebal/abi/vebal.json';
 import mainnet from '../../../config/mainnet';
+import { Multicaller3Viem } from '../../web3/multicaller-viem';
 
 export class UserSyncVebalLockBalanceService implements UserStakedBalanceService {
-    get chain() {
-        return networkContext.chain;
-    }
-
     private readonly veBalPoolId = '0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014';
+    private chain: Chain = 'MAINNET';
 
     public async initStakedBalances(stakingTypes: PrismaPoolStakingType[]): Promise<void> {
-        if (!stakingTypes.includes('VEBAL') && this.chain !== 'MAINNET') {
+        if (!stakingTypes.includes('VEBAL')) {
             return;
         }
 
@@ -42,17 +35,20 @@ export class UserSyncVebalLockBalanceService implements UserStakedBalanceService
 
         let operations: any[] = [];
         // for mainnet, we get the vebal balance form the vebal contract
-        const multicall = new Multicaller(networkContext.data.multicall, networkContext.provider, VeBalABI);
+        const multicall = new Multicaller3Viem('MAINNET', VeBalABI);
 
         let response = {} as {
             [userAddress: string]: {
                 balance: BigNumber;
-                locked: BigNumber[];
+                locked: {
+                    amount: BigNumber;
+                    end: BigNumber;
+                };
             };
         };
 
         for (const holder of subgraphVeBalHolders) {
-            multicall.call(`${holder.user}.locked`, networkContext.data.veBal!.address, 'locked', [holder.user]);
+            multicall.call(`${holder.user}.locked`, mainnet.veBal!.address, 'locked', [holder.user]);
 
             // so if we scheduled more than 100 calls, we execute the batch
             if (multicall.numCalls >= 100) {
@@ -73,7 +69,7 @@ export class UserSyncVebalLockBalanceService implements UserStakedBalanceService
 
         if (init) {
             operations.push(
-                prisma.prismaUserStakedBalance.deleteMany({ where: { staking: { type: 'VEBAL' }, chain: this.chain } }),
+                prisma.prismaUserStakedBalance.deleteMany({ where: { staking: { type: 'VEBAL' }, chain: 'MAINNET' } }),
             );
         }
 
@@ -84,16 +80,16 @@ export class UserSyncVebalLockBalanceService implements UserStakedBalanceService
                     create: {
                         id: `veBal-${veBalHolder.toLowerCase()}`,
                         chain: 'MAINNET',
-                        balance: formatFixed(response[veBalHolder].locked[0], 18),
-                        balanceNum: parseFloat(formatFixed(response[veBalHolder].locked[0], 18)),
+                        balance: formatFixed(response[veBalHolder].locked.amount, 18),
+                        balanceNum: parseFloat(formatFixed(response[veBalHolder].locked.amount, 18)),
                         userAddress: veBalHolder.toLowerCase(),
                         poolId: this.veBalPoolId,
                         tokenAddress: mainnet.veBal!.bptAddress,
                         stakingId: mainnet.veBal!.address,
                     },
                     update: {
-                        balance: formatFixed(response[veBalHolder].locked[0], 18),
-                        balanceNum: parseFloat(formatFixed(response[veBalHolder].locked[0], 18)),
+                        balance: formatFixed(response[veBalHolder].locked.amount, 18),
+                        balanceNum: parseFloat(formatFixed(response[veBalHolder].locked.amount, 18)),
                     },
                 }),
             );
@@ -101,8 +97,8 @@ export class UserSyncVebalLockBalanceService implements UserStakedBalanceService
 
         operations.push(
             prisma.prismaUserBalanceSyncStatus.upsert({
-                where: { type_chain: { type: 'VEBAL', chain: this.chain } },
-                create: { type: 'VEBAL', chain: this.chain, blockNumber: metadata.block.number },
+                where: { type_chain: { type: 'VEBAL', chain: 'MAINNET' } },
+                create: { type: 'VEBAL', chain: 'MAINNET', blockNumber: metadata.block.number },
                 update: { blockNumber: metadata.block.number },
             }),
         );
