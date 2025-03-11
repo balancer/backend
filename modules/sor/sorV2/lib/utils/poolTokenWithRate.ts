d86695e3e96771a5f504e939ce169ae64f2cb578
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2025-03-11 14:46:25 -0300

    Fix gyroE tokens rates

--- modules/sor/sorV2/lib/utils/poolTokenWithRate.ts
+++ modules/sor/sorV2/lib/utils/poolTokenWithRate.ts
@@ -1,4 +1,4 @@
-import { BigintIsh, Token, TokenAmount, WAD } from '@balancer/sdk';
+import { BigintIsh, DECIMAL_SCALES, MathSol, Token, TokenAmount, WAD } from '@balancer/sdk';
 import { BasePoolToken } from './basePoolToken';
 
 export class PoolTokenWithRate extends BasePoolToken {
@@ -20,5 +20,25 @@ export class PoolTokenWithRate extends BasePoolToken {
         this.amount = this.amount - amount;
         this.scale18 = (this.amount * this.scalar * this.rate) / WAD;
         return this;
+    }
+
+    static fromScale18Amount(): TokenAmount {
+        throw new Error('Use fromScale18AmountWithRate instead');
+    }
+
+    static fromScale18AmountWithRate(
+        token: Token,
+        scale18: bigint,
+        rate: bigint,
+        index: number,
+        divUp: boolean = true,
+    ): TokenAmount {
+        const scalar =
+            DECIMAL_SCALES[
+                (18 - token.decimals) as 0 | 1 | 2 | 3 | 4 | 5 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18
+            ];
+        const scaledRate = rate * scalar;
+        const amount = divUp ? MathSol.divUpFixed(scale18, scaledRate) : MathSol.divDownFixed(scale18, scaledRate);
+        return new PoolTokenWithRate(token, amount, index, rate);
     }
 }
