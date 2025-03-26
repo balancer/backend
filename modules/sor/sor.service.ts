16cfe9e4831006d203214774251fe3e93c25f42d
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2025-03-26 08:56:56 -0300

    Refactor sorPathService into sor.service

--- modules/sor/sor.service.ts
+++ modules/sor/sor.service.ts
@@ -1,45 +1,35 @@
+import * as Sentry from '@sentry/node';
+import { Address } from 'viem';
+
 import config from '../../config';
 import { GqlSorGetSwapPaths, QuerySorGetSwapPathsArgs } from '../../apps/api/gql/generated-schema';
-import { sorV2Service } from './sorV2/sorPathService';
-import { GetSwapsV2Input as GetSwapPathsInput } from './types';
+import { GetSwapsV2Input as GetSwapPathsInput, GraphTraversalConfig } from './types';
 import { getToken, getTokenAmountHuman, swapPathsZeroResponse } from './utils';
+import { mapSwapTypeToSwapKind, mapToSorSwapPaths } from './utils/mapping';
+import { sorGetPathsWithPools } from './sorV2/lib/static';
+import { getBasePoolsFromDb } from './utils/pool';
+import { PathWithAmount } from './sorV2/lib/path';
+import { isValidSwapRequest, validateTokens } from './utils/validation';
 
 export class SorService {
     async getSorSwapPaths(args: QuerySorGetSwapPathsArgs): Promise<GqlSorGetSwapPaths> {
         console.log('getSorSwaps args', JSON.stringify(args));
         const tokenIn = args.tokenIn.toLowerCase();
         const tokenOut = args.tokenOut.toLowerCase();
-        const amountToken = args.swapType === 'EXACT_IN' ? tokenIn : tokenOut;
         const emptyResponse = swapPathsZeroResponse(args.tokenIn, args.tokenOut, args.chain);
 
-        if (parseFloat(args.swapAmount) <= 0) {
+        // Early returns for invalid requests
+        if (!isValidSwapRequest(tokenIn, tokenOut, args.swapAmount, args.chain!)) {
             return emptyResponse;
         }
 
-        const wethIsEth = tokenIn === config[args.chain].eth.address || tokenOut === config[args.chain].eth.address;
-
-        // check if tokens addresses exist
-        try {
-            await getToken(tokenIn, args.chain!);
-            await getToken(tokenOut, args.chain!);
-        } catch (e: any) {
-            // Just log to console for parsing
-            console.log('Missing token for SOR request', `in: ${tokenIn}`, `out: ${tokenOut}`, args.chain);
+        if (!(await validateTokens(tokenIn, tokenOut, args.chain))) {
             return emptyResponse;
         }
 
-        // we return an empty response if tokenIn and tokenOut are the same
-        // also if tokenIn and tokenOut is weth/eth
-        if (
-            tokenIn === tokenOut ||
-            (wethIsEth && (tokenIn === config[args.chain].weth.address || tokenOut === config[args.chain].weth.address))
-        ) {
-            return emptyResponse;
-        }
-
-        // Use TokenAmount to help follow scaling requirements in later logic
-        // args.swapAmount is HumanScale
+        const amountToken = args.swapType === 'EXACT_IN' ? tokenIn : tokenOut;
         const amount = await getTokenAmountHuman(amountToken, args.swapAmount, args.chain!);
+        const wethIsEth = tokenIn === config[args.chain].eth.address || tokenOut === config[args.chain].eth.address;
 
         const getSwapPathsInput: Omit<GetSwapPathsInput, 'protocolVersion'> = {
             chain: args.chain!,
@@ -61,19 +51,30 @@ export class SorService {
             poolIds: args.poolIds ?? undefined,
         };
 
-        if (args.useProtocolVersion) {
-            return sorV2Service.getSorSwapPaths({
-                ...getSwapPathsInput,
-                protocolVersion: args.useProtocolVersion,
-            });
+        const mappedPaths = args.useProtocolVersion
+            ? this.getSorSwapPathsForVersion({
+                  ...getSwapPathsInput,
+                  protocolVersion: args.useProtocolVersion,
+              })
+            : this.getBestSwapPathVersion(getSwapPathsInput);
+
+        return mappedPaths;
+    }
+
+    public async getSorSwapPathsForVersion(input: GetSwapPathsInput, maxDepth = 4): Promise<GqlSorGetSwapPaths> {
+        const paths = await this.getSwapPathsWithRetry(input, maxDepth);
+
+        if (!paths) {
+            return swapPathsZeroResponse(input.tokenIn, input.tokenOut, input.chain);
         }
 
-        return this.getBestSwapPathVersion(getSwapPathsInput);
+        return mapToSorSwapPaths(paths, input.swapType, input.chain, input.protocolVersion as 2 | 3);
     }
 
     private async getBestSwapPathVersion(input: Omit<GetSwapPathsInput, 'protocolVersion'>) {
-        const swapBalancerV2 = await sorV2Service.getSorSwapPaths({ ...input, protocolVersion: 2 });
-        const swapBalancerV3 = await sorV2Service.getSorSwapPaths({ ...input, protocolVersion: 3 });
+        const swapBalancerV2 = await this.getSorSwapPathsForVersion({ ...input, protocolVersion: 2 });
+        const swapBalancerV3 = await this.getSorSwapPathsForVersion({ ...input, protocolVersion: 3 });
+
         if (input.swapType === 'EXACT_IN') {
             return parseFloat(swapBalancerV2.returnAmount) > parseFloat(swapBalancerV3.returnAmount)
                 ? swapBalancerV2
@@ -90,6 +91,84 @@ export class SorService {
                     : swapBalancerV3;
             }
         }
+    }
+
+    private async getSwapPathsWithRetry(
+        input: GetSwapPathsInput,
+        maxDepth: number = 4,
+    ): Promise<PathWithAmount[] | null> {
+        try {
+            const { pools: poolsFromDb, underlyingTokens } = await getBasePoolsFromDb(
+                input.chain,
+                input.protocolVersion,
+                input.considerPoolsWithHooks,
+                input.poolIds,
+            );
+
+            const tokenIn = await getToken(input.tokenIn as Address, input.chain);
+            const tokenOut = await getToken(input.tokenOut as Address, input.chain);
+            const swapKind = mapSwapTypeToSwapKind(input.swapType);
+
+            // retry with different max depth if no paths are found
+            let swapOptions = this.buildSwapOptions(maxDepth, input.graphTraversalConfig);
+            let paths = await sorGetPathsWithPools(
+                tokenIn,
+                tokenOut,
+                swapKind,
+                input.swapAmount.amount,
+                poolsFromDb,
+                underlyingTokens,
+                input.protocolVersion,
+                swapOptions,
+            );
+
+            if (!paths) {
+                swapOptions = this.buildSwapOptions(maxDepth + 1, input.graphTraversalConfig);
+                paths = await sorGetPathsWithPools(
+                    tokenIn,
+                    tokenOut,
+                    swapKind,
+                    input.swapAmount.amount,
+                    poolsFromDb,
+                    underlyingTokens,
+                    input.protocolVersion,
+                    swapOptions,
+                );
+            }
+
+            return paths;
+        } catch (err: any) {
+            this.logSwapPathError(err, input);
+            return null;
+        }
+    }
+
+    private buildSwapOptions(
+        maxNonBoostedPathDepth: number,
+        graphTraversalConfig?: GraphTraversalConfig,
+    ): { graphTraversalConfig: GraphTraversalConfig } {
+        return {
+            graphTraversalConfig: {
+                maxNonBoostedPathDepth,
+                ...graphTraversalConfig,
+            },
+        };
+    }
+    private logSwapPathError(err: any, input: GetSwapPathsInput): void {
+        console.log(
+            `SOR_V2_ERROR ${err.message} - tokenIn: ${input.tokenIn} - tokenOut: ${input.tokenOut} - swapAmount: ${input.swapAmount.amount} - swapType: ${input.swapType} - chain: ${input.chain}`,
+        );
+
+        Sentry.captureException(err.message, {
+            tags: {
+                service: 'sorV2',
+                tokenIn: input.tokenIn,
+                tokenOut: input.tokenOut,
+                swapAmount: input.swapAmount.amount,
+                swapType: input.swapType,
+                chain: input.chain,
+            },
+        });
     }
 }
 
---
+++ modules/sor/utils/validation.ts
@@ -0,0 +1,32 @@
+import { Chain } from '@prisma/client';
+import config from '../../../config';
+import { getToken } from '../utils';
+
+export function isValidSwapRequest(tokenIn: string, tokenOut: string, swapAmount: string, chain: Chain): boolean {
+    if (parseFloat(swapAmount) <= 0) {
+        return false;
+    }
+
+    const wethIsEth = tokenIn === config[chain].eth.address || tokenOut === config[chain].eth.address;
+
+    // Check if tokens are the same or if trying to swap between WETH/ETH
+    if (
+        tokenIn === tokenOut ||
+        (wethIsEth && (tokenIn === config[chain].weth.address || tokenOut === config[chain].weth.address))
+    ) {
+        return false;
+    }
+
+    return true;
+}
+
+export async function validateTokens(tokenIn: string, tokenOut: string, chain: Chain): Promise<boolean> {
+    try {
+        await getToken(tokenIn, chain);
+        await getToken(tokenOut, chain);
+        return true;
+    } catch (e) {
+        console.log('Missing token for SOR request', `in: ${tokenIn}`, `out: ${tokenOut}`, chain);
+        return false;
+    }
+}
