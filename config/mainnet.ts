510f37a817812ba910457002d45a270275dcb4b4
franz <franzns11@gmail.com>
2025-04-28 15:15:33 +0200

    euler APRs

--- config/mainnet.ts
+++ config/mainnet.ts
@@ -261,15 +261,6 @@ export default <NetworkData>{
                 tokenAddress: '0xf073bac22dab7faf4a3dd6c6189a70d54110525c',
             },
         ],
-        euler: {
-            subgraphUrl: 'https://api.thegraph.com/subgraphs/name/euler-xyz/euler-mainnet',
-            tokens: {
-                eUSDC: { address: '0xeb91861f8a4e1c12333f42dce8fb0ecdc28da716' },
-                eDAI: { address: '0xe025e3ca2be02316033184551d4d3aa22024d9dc' },
-                eUSDT: { address: '0x4d19f33948b99800b6113ff3e83bec9b537c85d2' },
-                eFRAX: { address: '0x5484451a88a35cd0878a1be177435ca8a0e4054e' },
-            },
-        },
         gearbox: {
             sourceUrl: 'https://charts-server.fly.dev/api/pools',
             tokens: {
