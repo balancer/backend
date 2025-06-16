c606276e51374738bd351e59dde19c81167408de
franz <franzns11@gmail.com>
2025-06-16 10:13:01 +0300

    add nested pool apr service

--- modules/aprs/handlers/index.ts
+++ modules/aprs/handlers/index.ts
@@ -5,6 +5,7 @@ export { AaveApiAprHandler } from './aave-api-apr';
 export { DynamicSwapFeeAprHandler } from './dynamic-swap-fee-apr';
 export { BeetswarsGaugeVotingAprHandler, MaBeetsAprHandler } from './mabeets-apr';
 export { MorphoRewardsAprHandler } from './morpho-apr-handler/morpho-rewards-apr-handler';
+export { NestedPoolAprHandler } from './nested-pool-apr-handler';
 
 // Add more handler exports as they are implemented
 // Example:
---
+++ modules/aprs/handlers/nested-pool-apr-handler/nested-pool-apr.service.ts
@@ -0,0 +1,81 @@
+import { prisma } from '../../../../prisma/prisma-client';
+import { collectsYieldFee } from '../../../pool/lib/pool-utils';
+import { AprHandler, PoolAPRData } from '../../types';
+import { PrismaPoolAprItem } from '@prisma/client';
+
+export class NestedPoolAprHandler implements AprHandler {
+    public getAprServiceName(): string {
+        return 'NestedPoolAprHandler';
+    }
+
+    public async calculateAprForPools(
+        pools: PoolAPRData[],
+    ): Promise<Omit<PrismaPoolAprItem, 'createdAt' | 'updatedAt'>[]> {
+        const aprItems: Omit<PrismaPoolAprItem, 'createdAt' | 'updatedAt'>[] = [];
+
+        for (const pool of pools) {
+            const protocolYieldFeePercentage = parseFloat(pool.dynamicData?.protocolYieldFee || '0');
+            const tokens = pool.tokens.filter((token) => {
+                // exclude the phantom bpt pool token itself
+                if (token.address === pool.address) {
+                    return false;
+                }
+            });
+
+            const poolIds = tokens.map((token) => token.nestedPool?.id || '');
+            // swap fee and IB yield is also earned on the parent pool
+            const aprItems = await prisma.prismaPoolAprItem.findMany({
+                where: {
+                    poolId: { in: poolIds },
+                    type: { in: ['IB_YIELD', 'SWAP_FEE_24H'] },
+                    chain: pool.chain,
+                },
+            });
+
+            for (const token of tokens) {
+                const tokenAprItems = aprItems.filter((item) => item.poolId === token.nestedPoolId);
+
+                if (
+                    !pool.dynamicData ||
+                    !token.nestedPool ||
+                    !token.nestedPool.type ||
+                    token.balanceUSD === 0 ||
+                    pool.dynamicData.totalLiquidity === 0
+                ) {
+                    continue;
+                }
+
+                for (const aprItem of tokenAprItems) {
+                    const itemId = `${pool.id}-${aprItem.id}`;
+                    //scale the apr as a % of total liquidity
+
+                    const apr = aprItem.apr * (token.balanceUSD / pool.dynamicData.totalLiquidity);
+                    let userApr = apr;
+
+                    if (
+                        collectsYieldFee(pool) &&
+                        // nested tokens/bpts that dont have a rate provider, we don't take any fees
+                        token.priceRate !== '1.0'
+                    ) {
+                        userApr = userApr * (1 - protocolYieldFeePercentage);
+                    }
+
+                    const title = aprItem.type === 'SWAP_FEE_24H' ? `${token.token.symbol} APR` : aprItem.title;
+
+                    aprItems.push({
+                        id: itemId,
+                        chain: pool.chain,
+                        poolId: pool.id,
+                        apr: userApr,
+                        title: title,
+                        type: aprItem.type,
+                        group: aprItem.group,
+                        rewardTokenAddress: aprItem.rewardTokenAddress,
+                        rewardTokenSymbol: aprItem.rewardTokenSymbol,
+                    });
+                }
+            }
+        }
+        return aprItems;
+    }
+}
