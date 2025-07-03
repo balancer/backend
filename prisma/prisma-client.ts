b64bc9f706703beefba7e28f7ba2ea6569a16ed8
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-07-03 18:01:38 +0200

    prisma debugging

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -19,6 +19,15 @@ export let prisma = new PrismaClient({
 //     },
 // });
 
+// let callCount = 0;
+// prisma.$use(async (params, next) => {
+//     console.log(JSON.stringify(params, null, 2), callCount++);
+//     // if (callCount > 8) {
+//     //     throw new Error(`Maximum number of Prisma calls exceeded (${callCount}) - ${JSON.stringify(params, null, 2)}`);
+//     // }
+//     return next(params);
+// });
+
 // prisma.$use(async (params, next) => {
 //     const before = Date.now()
 //     const result = await next(params)
