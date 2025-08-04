792326cd5f7259a434d8fe508ec54f773df846dc
franz <franzns11@gmail.com>
2025-08-04 20:43:34 +0200

    susds mainnet apr

--- modules/aprs/handlers/yb-tokens/yb-tokens-apr-handler.ts
+++ modules/aprs/handlers/yb-tokens/yb-tokens-apr-handler.ts
@@ -12,7 +12,6 @@ export class YbTokensAprHandler implements AprHandler {
     private ybTokensAprHandlers: YbAprHandlers;
 
     constructor(private aprConfig: YbAprConfig, chain: Chain) {
-        console.log('YbTokensAprHandler initialized with config:', aprConfig);
         this.ybTokensAprHandlers = new YbAprHandlers(this.aprConfig, chain);
     }
 
