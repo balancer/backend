47278944f113029c4ede54250a24af2d640133c3
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-09-02 14:48:00 +0200

    updated fetch example

--- modules/yb-tokens/examples/fetch.ts
+++ modules/yb-tokens/examples/fetch.ts
@@ -1,10 +1,15 @@
 import { YbAprHandlers } from '../handlers';
-import mainnet from '../../../config/mainnet';
+import cnfg from '../../../config';
 
-const config = mainnet.aprHandlers.ybAprHandler;
-const handler = new YbAprHandlers(config!, 'MAINNET');
+const main = async (_chain: string) => {
+    const chain = _chain as keyof typeof cnfg;
+    const config = cnfg[chain].aprHandlers.ybAprHandler;
+    const handler = new YbAprHandlers(config!, chain);
 
-handler
-    .fetchAprsFromAllHandlers()
+    return handler.fetchAprsFromAllHandlers();
+};
+
+main(process.argv[2])
     .then(console.log)
+    .catch(console.log)
     .finally(() => process.exit(0));
