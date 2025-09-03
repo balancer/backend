85bc8b974e0eb3c2e551b3f6ffa985dc91293d02
gmbronco <83549293+gmbronco@users.noreply.github.com>
2025-09-03 13:15:34 +0200

    removing not fetched tokens from the db

--- modules/yb-tokens/repository.ts
+++ modules/yb-tokens/repository.ts
@@ -17,7 +17,13 @@ export class YbTokenRepository {
      * Store token yields in database
      */
     async storeTokens(chain: Chain, tokenAprs: YbToken[]): Promise<void> {
-        const operations = tokenAprs.map((tokenApr) => ({
+        const existingTokens = await prisma.prismaYbToken
+            .findMany({ where: { chain } })
+            .then((records) => records.map((r) => r.address));
+        const fetchedTokens = new Set(tokenAprs.map((t) => t.address));
+        const removeTokens = existingTokens.filter((x) => !fetchedTokens.has(x));
+
+        const upserts = tokenAprs.map((tokenApr) => ({
             where: {
                 address_chain: {
                     address: tokenApr.address.toLowerCase(),
@@ -37,7 +43,17 @@ export class YbTokenRepository {
         }));
 
         await prismaBulkExecuteOperations(
-            operations.map((operation) => prisma.prismaYbToken.upsert(operation)),
+            [
+                ...upserts.map((upsert) => prisma.prismaYbToken.upsert(upsert)),
+                prisma.prismaYbToken.deleteMany({
+                    where: {
+                        chain,
+                        address: {
+                            in: removeTokens,
+                        },
+                    },
+                }),
+            ],
             true,
         );
     }
