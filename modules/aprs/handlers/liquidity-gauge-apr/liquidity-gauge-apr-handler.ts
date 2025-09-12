a40b89783019efb59de3aa1802a8472b6717d017
franz <franzns11@gmail.com>
2025-09-12 13:49:49 +0200

    fix edge case for gauge apr

--- modules/aprs/handlers/liquidity-gauge-apr/liquidity-gauge-apr-handler.ts
+++ modules/aprs/handlers/liquidity-gauge-apr/liquidity-gauge-apr-handler.ts
@@ -93,7 +93,7 @@ export class LiquidityGaugeAprHandler implements AprHandler {
                     type: isVeBalemissions ? PrismaPoolAprType.VEBAL_EMISSIONS : PrismaPoolAprType.STAKING,
                 };
 
-                const adjustedGaugeTvl = gaugeTvl === 0 ? 1 : gaugeTvl; // Avoid division by zero
+                const adjustedGaugeTvl = !gaugeTvl || gaugeTvl === 0 ? 1 : gaugeTvl; // Avoid division by zero
 
                 // veBAL rewards have a min and max, we create two items for them
                 if (isVeBalemissions && (pool.chain === 'MAINNET' || gauge.version === 2)) {
