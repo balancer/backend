03cef6c7fc4ef2ae9c17bca1952f80ef6fd36037
franz <franzns11@gmail.com>
2025-09-12 09:48:32 +0200

    simplify

--- modules/aprs/handlers/swap-fee-apr/swap-fee-apr-handler.ts
+++ modules/aprs/handlers/swap-fee-apr/swap-fee-apr-handler.ts
@@ -41,38 +41,43 @@ export class SwapFeeAprHandler implements AprHandler {
         const chain = pools[0].chain;
         const yesterday = daysAgo(1);
 
-        // Fetch the swap fees for the last 30 days
+        // Fetch the swap fees for the last 24 hours
         const swapFeeData = await prisma.$queryRawUnsafe<PoolSwapFeeData[]>(query(chain, yesterday));
 
         // Map the swap fee data to the pool id
-        const swapFeeDataMap = swapFeeData.reduce(
-            (acc, data) => {
-                acc[data.poolId] = data;
-                return acc;
-            },
-            {} as Record<string, PoolSwapFeeData>,
-        );
+        const swapFeeDataMap = swapFeeData.reduce((acc, data) => {
+            acc[data.poolId] = data;
+            return acc;
+        }, {} as Record<string, PoolSwapFeeData>);
 
         const aprItems: Omit<PrismaPoolAprItem, 'createdAt' | 'updatedAt'>[] = ([] = pools
             .flatMap((pool) => {
                 if (!pool.dynamicData) return null;
 
-                const dynamicApr = dynamicSwapFeeAprCalculation(
+                let protocolFee = pool.dynamicData.protocolSwapFee;
+
+                if (pool.type === 'GYROE') {
+                    protocolFee = pool.dynamicData.protocolYieldFee || '0';
+                }
+
+                if (pool.version === 3) {
+                    protocolFee = pool.dynamicData.aggregateSwapFee;
+                }
+
+                if (pool.dynamicData.isInRecoveryMode || pool.type === 'LIQUIDITY_BOOTSTRAPPING') {
+                    protocolFee = '0';
+                }
+
+                const dynamicApr = swapFeeCalculation(
                     pool.dynamicData.totalLiquidity,
                     swapFeeDataMap[pool.id].dynamic_fees_24h,
-                    parseFloat(pool.dynamicData.aggregateSwapFee),
-                    pool.dynamicData.isInRecoveryMode,
+                    parseFloat(protocolFee),
                 );
 
-                const swapApr = swapFeeAprCalculation(
+                const swapApr = swapFeeCalculation(
                     pool.dynamicData.totalLiquidity,
                     swapFeeDataMap[pool.id].fees_24h,
-                    parseFloat(pool.dynamicData.protocolSwapFee),
-                    parseFloat(pool.dynamicData.aggregateSwapFee),
-                    parseFloat(pool.dynamicData.protocolYieldFee || '0'),
-                    pool.dynamicData.isInRecoveryMode,
-                    pool.type,
-                    pool.protocolVersion as 1 | 2 | 3,
+                    parseFloat(protocolFee),
                 );
 
                 const surplusApr = surplusAprCalculation(
@@ -121,17 +126,8 @@ export class SwapFeeAprHandler implements AprHandler {
     }
 }
 
-const dynamicSwapFeeAprCalculation = (
-    totalLiquidity: number,
-    swapFees: number,
-    protocolFee: number,
-    isInRecoveryMode: boolean,
-) => {
+const swapFeeCalculation = (totalLiquidity: number, swapFees: number, protocolFee: number) => {
     let apr_24h = 0;
-
-    if (isInRecoveryMode) {
-        protocolFee = 0;
-    }
 
     if (totalLiquidity > 0 && swapFees) {
         apr_24h = ((swapFees * 365) / totalLiquidity) * (1 - protocolFee);
@@ -142,41 +138,6 @@ const dynamicSwapFeeAprCalculation = (
     }
 
     return apr_24h;
-};
-
-const swapFeeAprCalculation = (
-    totalLiquidity: number,
-    swapFees: number,
-    protocolSwapFee: number,
-    aggregateSwapFee: number,
-    protocolYieldFee: number,
-    isInRecoveryMode: boolean,
-    poolType: PrismaPoolType,
-    protocolVersion: 1 | 2 | 3,
-) => {
-    const apr = totalLiquidity > 0 ? (swapFees * 365) / totalLiquidity : 0;
-
-    let protocolFee = protocolSwapFee;
-
-    if (poolType === 'GYROE') {
-        protocolFee = protocolYieldFee;
-    }
-
-    if (protocolVersion === 3) {
-        protocolFee = aggregateSwapFee;
-    }
-
-    if (isInRecoveryMode || poolType === 'LIQUIDITY_BOOTSTRAPPING') {
-        protocolFee = 0;
-    }
-
-    let userApr = apr * (1 - protocolFee);
-
-    if (userApr > MAX_DB_INT) {
-        userApr = 0;
-    }
-
-    return userApr;
 };
 
 const surplusAprCalculation = (totalLiquidity: number, surplus24h: number) => {
