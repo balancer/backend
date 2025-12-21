6d50b0faa2027ac6e3dd3fc75a2644926867535b
franz <franzns11@gmail.com>
2025-12-21 10:09:18 +0100

    deprecate mode

--- modules/network/mode.ts
+++ modules/network/mode.ts
@@ -1,12 +1,17 @@
 import { NetworkConfig } from './network-config-types';
 import { UserSyncGaugeBalanceService } from '../user/lib/user-sync-gauge-balance.service';
 import config from '../../config';
-import { activeChainWorkerJobsGeneric, activeChainWorkerJobsV2, vebalWorkerJobs } from './worker-jobs';
+import {
+    activeChainWorkerJobsGeneric,
+    activeChainWorkerJobsV2,
+    deprecatedChainWorkerJobs,
+    vebalWorkerJobs,
+} from './worker-jobs';
 
 export const modeNetworkData = config.MODE;
 
 export const modeNetworkConfig: NetworkConfig = {
     data: modeNetworkData,
     userStakedBalanceServices: [new UserSyncGaugeBalanceService()],
-    workerJobs: [...activeChainWorkerJobsGeneric, ...activeChainWorkerJobsV2, ...vebalWorkerJobs],
+    workerJobs: [...deprecatedChainWorkerJobs, ...vebalWorkerJobs],
 };
