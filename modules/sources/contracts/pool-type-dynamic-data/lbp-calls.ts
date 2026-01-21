b2573213d52e68b72082f279597400df2d8f37ef
franz <franzns11@gmail.com>
2026-01-21 13:56:12 +0100

    fix index in lbp sync

--- modules/sources/contracts/pool-type-dynamic-data/lbp-calls.ts
+++ modules/sources/contracts/pool-type-dynamic-data/lbp-calls.ts
@@ -64,7 +64,7 @@ export const lbpCalls = (poolAddress: string, vaultAddress: string): ViemMultica
         abi,
         functionName: 'getLBPoolDynamicData',
         parser: (result: DynamicData, results: any, index: number) => {
-            const immutableData = results[index - 1].result as ImmutableData;
+            const immutableData = results[index - 2].result as ImmutableData;
             const tokens = immutableData.tokens;
 
             const poolToken = tokens.map((token, index) => ({
