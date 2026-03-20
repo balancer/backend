7d8a646beb119197628aaa9f5da01f236bfe6bfd
franz <franzns11@gmail.com>
2026-03-20 09:40:50 +0100

    always sync LBPs

--- modules/controllers/pool-controller.ts
+++ modules/controllers/pool-controller.ts
@@ -169,7 +169,11 @@ export function PoolController(tracer?: any) {
 
             let poolsToSync: V3JoinedSubgraphPool[] = [];
             if (syncNewPoolsOnly) {
-                poolsToSync = pools.filter((pool) => !dbIds.includes(pool.id));
+                // always sync LBPs and FixedLBPs factories, as they can be created without being registered in the vault
+                poolsToSync = pools.filter(
+                    (pool) =>
+                        pool.factory.type === 'LBP' || pool.factory.type === 'FixedLBP' || !dbIds.includes(pool.id),
+                );
             } else {
                 poolsToSync = pools;
             }
