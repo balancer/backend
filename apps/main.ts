a186cf30fdf6e4272c8823bac488633a435586b5
Franz <franzns11@gmail.com>
2026-06-19 14:24:31 +0200

    add direct dev sdk

--- apps/main.ts
+++ apps/main.ts
@@ -2,6 +2,8 @@ import './sentry';
 import { startApiServer } from './api/server';
 import { startWorkerServer } from './worker/server';
 import { startSchedulerServer } from './scheduler/server';
+import { install } from '@direct.dev/sdk';
+install();
 
 if (process.env.WORKER === 'true') {
     startWorkerServer();
